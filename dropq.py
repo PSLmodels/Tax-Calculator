e38072ff8763de5d591129dc3965cc5b8b6a4962
Ilia Kurenkov <ilia.kurenkov@gmail.com>
2014-08-12 16:29:38 -0400

    added pseudocode in comments and function stubs

---
+++ dropq.py
@@ -0,0 +1,41 @@
+
+# load input data
+
+# load planX
+
+# read planY
+
+# use python dict update to incorporate planY into planX
+
+# one loop iteration (do this for every year)
+# for every row in database:
+# we take our data and age it a year to array djfo ?
+# using this data we calculate tax1 using calc procedure
+# add $1 to income element of djfo
+# we calculate tax2 using calc procedure and modified djfo
+# if tax2 != tax1, set records to active
+# we calculate tax3 using tax2 and modified djfo
+# pick 3 random active records and set them to zero
+
+# return table of tax0
+
+# return table of tax3 - tax1
+
+# go to next row
+
+
+## function stubs
+
+def ohare(tax_payer_row, year):
+	'''This function ages items in data by a year.
+	Maybe a more expressive name would be nice for this.
+	'''
+	return year
+
+def calculate_tax(tax_payer_row, year, plan):
+	'''Actually, not very sure about this one. It's supposed to calculate
+	tax and other variables for a given tax payer under the "plan".
+	Not sure what year is doing here.
+	As with previous function, details have not yet been fleshed out.
+	'''
+	return tax
