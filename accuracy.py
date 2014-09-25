02e2bdbf455d477b9a38389672809ce331e1ac37
Ilia Kurenkov <ilia.kurenkov@gmail.com>
2014-09-25 00:07:24 -0400

    reworked accuracy with use of HDF5 for SAS file

--- accuracy.py
+++ accuracy.py
@@ -1,28 +1,71 @@
 import numpy as np
 import pandas as pd
-
-import cPickle
-import re
+import h5py as h5
 
 import os
+import os.path as op
 
 
-def check_accuracy(py_answer, gold_std):
-    '''The older way of computing accuracy.
-    Takes two dictionaries, py_answer with python script's output variables
-    and gold_std with Dan's C-codes.
+_VARIABLES_OF_INTEREST = {
+    'AGI.csv' : ['c00100'],
+    'ItemDed.csv' : ['c04470']
+}
+
+
+def main(sas_codes_path, rerun=False):
+    if rerun:
+        import translation
+        translation.Test(True)
+    # for now output dir for translation has to be set up separately
+    # TO-DO: automate this directory setup
+    gold_std = h5.File(sas_codes_path)
+    errors = compute_error(_VARIABLES_OF_INTEREST, 'py_output', gold_std)
+    errors.to_csv('error_values.csv', 
+        header=['Error'],
+        index_label='Variable'
+        )
+
+
+def compute_error(to_look_at, out_dir, gold_std):
+    '''Computes the "error" by looking at a specified list of variables and
+    reporting 
     '''
-    acc_dict = {}
-    for key in py_answer:
-        if key in gold_std:
-            print 'Found {} in gold standard'.format(key)
-            test_length = float(len(py_answer[key]))
-            gold_array = np.array(gold_std[key])
-            print 'Array length is equal? {}'.format(len(gold_array) == test_length)
-            n_correct = sum(py_answer[key] == gold_array)
-            # n_correct = sum(np.in1d(py_answer[key], gold_array))
-            acc_dict[key] = n_correct / test_length
-    return acc_dict
+    error = {}
+    for file_name in to_look_at:
+        file_vars = pd.read_csv(op.join(out_dir, file_name))
+        for var_name in to_look_at[file_name]:
+            per_taxpayer_diff = file_vars[var_name] - gold_std[var_name]
+            error[var_name] = np.absolute(per_taxpayer_diff).sum()
+
+    return pd.DataFrame.from_dict(error, orient='index')
+
+
+def mismatching_records(gold_std, variable, file_path):
+    '''
+    Given a gold standard dictionary, a variable name and a path to the file
+    where this variable is stored returns an array of indices for taxpayer
+    records where python's output does not match  that of SAS.
+    '''
+    py_answer = pd.read_csv(file_path)[variable]
+    mismatches = py_answer != gold_std[variable]
+    all_indices = np.arange(len(mismatches))
+    return all_indices[mismatches]
+
+
+def report_accuracy(python_output_dir, sas_codes, indx):
+    '''Our current implementation of accuracy testing.
+    expects directory with results of sameer's python translation as well as 
+    a c-codes dictionary and the index which to use
+    '''
+    accuracies = {}
+    for var_file_path in gen_file_paths(python_output_dir):
+        var_df = pd.read_csv(var_file_path)
+
+        accuracies.update(var_df.iloc[indx].to_dict())
+
+    accuracies = merge_dicts(sas_codes, indx, accuracies)
+    accuracy_df = pd.DataFrame.from_dict(accuracies, orient='index')
+    accuracy_df.to_csv('accuracy.csv')
 
 
 def gen_file_paths(dir_name, filter_func=None):
@@ -38,78 +81,35 @@ def gen_file_paths(dir_name, filter_func=None):
     :type filter_func: None by default, function if passed
     :returns: iterator over paths for files in *dir_name*
     '''
-    file_paths = tuple(os.path.join(dir_name, file_name) 
+    file_paths = tuple(op.join(dir_name, file_name) 
         for file_name in os.listdir(dir_name))
     if filter_func:
         return filter(filter_func, file_paths)    
     return file_paths
 
 
-def main(translation_output_dir, pickled_C_codes):
-    '''Our current implementation of accuracy testing.
-
-    NOTE: DUE TO MISSING DATA IN THE C-CODES FILE, THIS IMPLEMENTATION DOES NOT
-    YET WORK.
-
-    Takes name for folder containing output csvs from the python translation
-    and file path to pickled C-codes (correct answers).
-    Writes per variable comparison statistics to a csv, prints following info:
-    - # of mismatches in length between variables
-    - # of variables traversed
-    - # of variables not found in C-codes file
+def merge_dicts(sas_codes, taxpayer, python_codes):
     '''
-    report_msg = ("# of mismatches in length: {0}\n"
-        "total # variables processed: {1}\n"
-        "couldn't find {2} variables in C-Codes file\n"
-        "length of accuracy dict: {3}")
-
-    simplifier = re.compile('\W')    
-    array = np.array
-    sum = np.sum
-    # start by unpickling the c-codes file
-    with open(pickled_C_codes) as c_code_file:
-        c_codes = cPickle.load(c_code_file)
-    # print c_codes.keys()
-    different_lens = 0
-    vars_processed, vars_not_found = 0, 0
-    accuracies = {}
-    # loop over csv files in output directory
-    for var_file_path in gen_file_paths(translation_output_dir):
-        var_df = pd.read_csv(var_file_path)
-        for variable in var_df:
-            vars_processed += 1
-            variable_clean = simplifier.sub('', variable)
-            # We check if a cleaned up version of var name is in c_codes file
-            if variable_clean in c_codes:
-                # only need to convert c_code column to array
-                py_ansr = var_df[variable]
-                # below line raises errors if c-codes variable only contains
-                # empty strings. Will have to check with Dan about how
-                # to deal with them. IK.
-                gold_std = array(map(float, c_codes[variable_clean]))
-                py_ansr_len = len(py_ansr)
-                # compare arrays element-wise for equality, convert number of 
-                # matches to float for subsequent division
-                n_correct = float(sum(py_ansr == gold_std))
-
-                different_lens += int(py_ansr_len == len(gold_std))                
-
-                accuracies[variable] = n_correct / py_ansr_len
-            else:
-                # in case our var isn't in C-codes file
-                vars_not_found += 1
-
-    print report_msg.format(different_lens, vars_processed, 
-        vars_not_found, len(accuracies))
-
-    # clear C-Codes file
-    c_codes = None
-    # create a pandas DataFrame 
-    accuracy_df = pd.DataFrame.from_dict(accuracies, orient='index')
-    #write it to csv, creating column names
-    accuracy_df.to_csv('accuracy.csv', colums=('VarName', 'RatioCorrect'))
+    Combines SAS output for a taxpayer with the corresponding python output.
+    '''
+    result = {}
+    for variable in sas_codes:
+        if variable in python_codes:
+            result[key] = (sas_codes[key], python_codes[key])
+        else:
+            result[key] = (sas_codes[key], '')
+    return result
 
 
 if __name__ == '__main__':
-    main('test/gold_csv', 'test/withc.pickle')
 
+    from argparse import ArgumentParser
+    parser = ArgumentParser('Testing script')
+    parser.add_argument('SASCODESPATH',
+        help='path to HDF5 file with SAS codes')
+    parser.add_argument('rerun', nargs='?', default=False,
+        help=('pass any integer other than 0 to rerun taxcalc, '
+            'otherwise do not pass anything'))
+
+    cmd_input = parser.parse_args()
+    main(cmd_input.SASCODESPATH, bool(cmd_input.rerun))
