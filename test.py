0b2686141ef1cfdae4773f337344f892e2341755
Ilia Kurenkov <ilia.kurenkov@gmail.com>
2014-11-04 09:37:24 -0500

    write-out just once CSV

--- test.py
+++ test.py
@@ -16,79 +16,6 @@ def to_csv(fname, df):
     """
     df.to_csv(fname, float_format= '%1.3f', sep=',', header=True, index=False)
 
-
-def split_into_CSVs(calculatedDF):
-    filenames_cols = {
-    'FilingStatus' : ['_sep', '_txp'],
-    'Adj' : ['_feided', 'c02900'],
-    'CapGains' : ['c23650', 'c01000', 'c02700', '_ymod1', '_ymod2',
-                               '_ymod3', '_ymod'],
-    'SSBenefits' : ['c02500', 'e02500'],
-    'AGI' : ['c02650', 'c00100', '_agierr', '_posagi',
-                              '_ywossbe', '_ywossbc', '_prexmp', 'c04600'],
-    'ItemDed' : ['c17750', 'c17000', '_sit1', '_sit', '_statax', 'c18300', 'c37703',
-             'c20500', 'c20750', 'c20400', 'c19200', 'c20800', '_lim50',
-             '_lim30', 'c19700', 'c21060', '_phase2', '_itemlimit',
-             '_nonlimited', '_limitratio', 'c04470', '_itemlimit', '_dedpho',
-             '_dedmin', 'c21040'],
-    'EIFICA' : ['_sey', '_fica', '_setax', '_seyoff', 'c11055', '_earned'],
-    'StdDed' : ['c15100', 'c04100', '_numextra', '_txpyers', 'c04200', 'c15200',
-              '_standard', '_othded', 'c04100', 'c04200', '_standard',
-              'c04500', 'c04800', 'c60000', '_amtstd', '_taxinc', '_feitax',
-              '_oldfei'],
-    'XYZD' : ['_xyztax', 'c05200'],
-    'NonGain' : ['_cglong', '_noncg'],
-    'TaxGains' : ['e00650', '_hasgain', '_dwks5', 'c24505', 'c24510', '_dwks9',
-              'c24516', 'c24580', 'c24516', '_dwks12', 'c24517', 'c24520',
-              'c24530', '_dwks16', '_dwks17', 'c24540', 'c24534', '_dwks21',
-              'c24597', 'c24598', '_dwks25', '_dwks26', '_dwks28', 'c24610',
-              'c24615', '_dwks31', 'c24550', 'c24570', '_addtax', 'c24560',
-              '_taxspecial', 'c05100', 'c05700', 'c59430', 'c59450', 'c59460',
-              '_line17', '_line19', '_line22', '_line30', '_line31',
-              '_line32', '_line36', '_line33', '_line34', '_line35',
-              'c59485', 'c59490', 'c05700', '_s1291', '_parents', 'c05750',
-              '_taxbc'],
-    'MUI' : ['c05750'],
-    'AMTI' : ['c62720', 'c60260', 'c63100', 'c60200', 'c60240', 'c60220',
-              'c60130', 'c62730', '_addamt', 'c62100', '_cmbtp', '_edical',
-              '_amtsepadd', 'c62600', '_agep', '_ages', 'c62600', 'c62700',
-              '_alminc', '_amtfei', 'c62780', 'c62900', 'c63000', 'c62740',
-              '_ngamty', 'c62745', 'y62745', '_tamt2', '_amt5pc', '_amt15pc',
-              '_amt25pc', 'c62747', 'c62755', 'c62770', '_amt', 'c62800',
-              'c09600', '_othtax', 'c05800'],
-    'F2441' : ['_earned', 'c32880', 'c32890', '_ncu13', '_dclim', 'c32800'],
-    'DepCareBen' : ['_seywage', 'c33465', 'c33470', 'c33475', 'c33480', 'c32840',
-              'c32800', 'c33000'],
-    'ExpEarnedInc' : ['_tratio', 'c33200', 'c33400', 'c07180'],
-    'RateRed' : ['c07970', 'c05800', 'c59560'],
-    'NumDep' : ['_ieic', 'EICYB1', 'EICYB2', 'EICYB3', '_modagi',
-              'c59660', '_val_ymax', '_preeitc', '_val_rtbase',
-              '_val_rtless', '_dy'],
-    'ChildTaxCredit' : ['c11070', 'c07220', 'c07230', '_precrd', '_num', '_nctcr',
-              '_precrd', '_ctcagi'],
-    'AmOppCr' : ['c87482', 'c87487', 'c87492', 'c87497', 'c87483', 'c87488',
-              'c87493', 'c87498', 'c87521'],
-    'LLC' : ['c87540', 'c87550', 'c87530'],
-    'RefAmOpp' : ['c87654', 'c87656', 'c87658', 'c87660', 'c87662', 'c87664',
-              'c87666', 'c10960', 'c87668', 'c87681'],
-    'NonEdCr' : ['c87560', 'c87570', 'c87580', 'c87590', 'c87600', 'c87610',
-              'c87620', '_ctc1', '_ctc2', '_regcrd', '_exocrd', '_ctctax',
-              'c07220'],
-    'AddCTC' : ['c82940', 'c82925', 'c82930', 'c82935', 'c82880', 'h82880',
-              'c82885', 'c82890', 'c82900', 'c82905', 'c82910', 'c82915',
-              'c82920', 'c82937', 'c11070', 'e59660', '_othadd'],
-    'F5405' : ['c64450'],
-    'C1040' : ['c07100', 'y07100', 'x07100', 'c08795', 'c08800', 'e08795',
-              'c09200'],
-    'DEITC' : ['c59680', 'c59700', 'c59720', '_comb', 'c07150', 'c10950'],
-    'SOIT' : ['c10300', '_eitc']
-    }
-
-    for file_name in filenames_cols:
-        relevant_columns = filenames_cols[file_name]
-        relevant_df = calculatedDF[relevant_columns]
-        to_csv(file_name + '.csv', relevant_df)
-
     
 def run(puf=True):
     """
@@ -141,7 +68,7 @@ def run(puf=True):
     calculated = concat([calculated, df_C1040], axis=1)
     calculated = concat([calculated, DEITC()], axis=1)
     calculated = concat([calculated, SOIT(_eitc)], axis=1)
-    split_into_CSVs(calculated)
+    to_csv(calculated)
 
 
 if __name__ == '__main__':
