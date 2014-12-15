0cc23d3b291254ecc631d62322023af03426f66b
talumbau <tj.alumbaugh@continuum.io>
2014-12-15 11:26:09 -0600

    add versioning with versioneer

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
+import versioneer
+versioneer.VCS = 'git'
+versioneer.versionfile_source = 'taxcalc/_version.py'
+versioneer.versionfile_build = 'taxcalc/_version.py'
+versioneer.tag_prefix = '' # tags are like 1.2.0
+versioneer.parentdir_prefix = 'taxcalc-' # dirname like 'taxcalc-1.2.0'
+
 try:
     from setuptools import setup
 except ImportError:
@@ -6,6 +13,9 @@ except ImportError:
 with open('README.md') as f:
         longdesc = f.read()
 
+version = versioneer.get_version()
+cmdclass = versioneer.get_cmdclass()
+
 config = {
     'description': 'Tax Calculator',
     'url': 'https://github.com/OpenSourcePolicyCenter/Tax-Calculator',
@@ -13,7 +23,8 @@ config = {
     'description':'taxcalc',
     'long_description':longdesc,
     'install_requires': ["pandas", "numpy"],
-    'version': '0.0',
+    'version': version,
+    'cmdclass': cmdclass,
     'license': 'MIT',
     'packages': ['taxcalc'],
     'include_package_data': True,
