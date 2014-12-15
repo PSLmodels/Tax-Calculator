93ba72d8c247663c27f1ad8d95681f31b7597460
talumbau <tj.alumbaugh@continuum.io>
2014-12-15 11:26:09 -0600

    add versioning with versioneer

--- taxcalc/__init__.py
+++ taxcalc/__init__.py
@@ -2,3 +2,7 @@ from .calculate import *
 from .parameters import *
 from .puf import *
 from .utils import *
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
