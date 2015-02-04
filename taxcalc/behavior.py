b45a7426851f17309e8b9bb96ae4c11f5513a7e6
Matt Jensen <matt.jensen@aei.org>
2015-02-04 18:03:47 -0500

    moved behavioral example to the parent directory

--- taxcalc/behavior.py
+++ taxcalc/behavior.py
@@ -8,19 +8,16 @@ class Behavior(object):
 		self.inc_effect = inc_effect
 
 	@property
-	def behavior(self, ):
+	def calc(self):
 		"""
 	    Modify plan Y records to account for micro-feedback effect that arrise 
 	    from moving from plan X to plan Y. 
 	    """
 		# Calculate marginal tax rates for plan x and plan y.
 		mtrX = self.calcX.mtr('e00200')
-		print 'mtrX'
-		print mtrX
 
 		mtrY = self.calcY.mtr('e00200')
-		print 'mtrY'
-		print mtrY
+
 		# Calculate the percent change in after-tax rate.
 		pct_diff_atr = ((1-mtrY) - (1-mtrX))/(1-mtrX)
 
@@ -28,13 +25,10 @@ class Behavior(object):
 		# Calculate the magnitude of the substitution and income effects. 
 		substitution_effect = (self.elast_wrt_atr * pct_diff_atr * 
 								(self.calcX.c05200))
-		print 'substitution_effect'
-		print substitution_effect
 
 
 		income_effect = self.inc_effect * (self.calcY.c05200 - self.calcX.c05200)
-		print 'income_effect'
-		print income_effect
+
 		delta_inc = np.where(self.calcY.c00100 > 0 ,
 					substitution_effect + income_effect, 0)
 
