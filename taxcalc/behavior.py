730096db80e2e3a760835c0952e28aab03a18840
Matt Jensen <matt.jensen@aei.org>
2015-02-21 17:13:29 -0500

    recalculate taxes between substitution effect and income effect -- use _ospctax rather than c05200.

--- taxcalc/behavior.py
+++ taxcalc/behavior.py
@@ -1,35 +1,10 @@
 import numpy as np
 import copy
 
-def behavior(calcX, calcY, elast_wrt_atr = 0.4, inc_effect = 0.15):
-	"""
-    Modify plan Y records to account for micro-feedback effect that arrise 
-    from moving from plan X to plan Y. 
-    """
-
-	# Calculate marginal tax rates for plan x and plan y.
-	mtrX = calcX.mtr('e00200')
-
-	mtrY = calcY.mtr('e00200')
-
-	# Calculate the percent change in after-tax rate.
-	pct_diff_atr = ((1-mtrY) - (1-mtrX))/(1-mtrX)
-
-
-	# Calculate the magnitude of the substitution and income effects. 
-	substitution_effect = (elast_wrt_atr * pct_diff_atr * 
-							(calcX.c05200))
-
-
-	income_effect = inc_effect * (calcY.c05200 - calcX.c05200)
-
+def update_income(effect, calcY):  
 	delta_inc = np.where(calcY.c00100 > 0 ,
-				substitution_effect + income_effect, 0)
+				effect, 0)
 
-	# TODO Replace c05200 with _ospctax.
-	# TODO Currently we are not recalculating tax with the substitution
-	# effect before calculating the income effect. Should we be? 
-	
 	# Attribute the behavioral effects across itemized deductions, 
 	# wages, and other income. 
 
@@ -48,16 +23,51 @@ def behavior(calcX, calcY, elast_wrt_atr = 0.4, inc_effect = 0.15):
 	delta_itemized = (delta_inc * _itemized / 
 						(calcY.c00100 + _itemized + .001))
 
-	calcY_behavior = copy.deepcopy(calcY)
+	calcY.e00200 = calcY.e00200 + delta_wages
 
-	calcY_behavior.e00200 = calcY_behavior.e00200 + delta_wages
+	calcY.e00300 = calcY.e00300 + delta_other_inc
 
-	calcY_behavior.e00300 = calcY_behavior.e00300 + delta_other_inc
-
-	calcY_behavior.e19570 = np.where(_itemized > 0, 
-							calcY_behavior.e19570 + delta_itemized, 0)
+	calcY.e19570 = np.where(_itemized > 0, 
+							calcY.e19570 + delta_itemized, 0)
 	#TODO, we should create a behavioral modification 
 	# variable instead of using e19570
 
+	calcY.calc_all()
+
+	return calcY
+
+
+def behavior(calcX, calcY, elast_wrt_atr=0.4, inc_effect=0.15, update_income=update_income):
+	"""
+    Modify plan Y records to account for micro-feedback effect that arrise 
+    from moving from plan X to plan Y. 
+    """
+
+	# Calculate marginal tax rates for plan x and plan y.
+	mtrX = calcX.mtr('e00200')
+
+	mtrY = calcY.mtr('e00200')
+
+	# Calculate the percent change in after-tax rate.
+	pct_diff_atr = ((1-mtrY) - (1-mtrX))/(1-mtrX)
+
+	calcY_behavior = copy.deepcopy(calcY)
+
+	# Calculate the magnitude of the substitution and income effects. 
+	substitution_effect = (elast_wrt_atr * pct_diff_atr * 
+							(calcX._ospctax))
+
+
+	calcY_behavior = update_income(substitution_effect, calcY_behavior)
+
+
+	income_effect = inc_effect * (calcY_behavior._ospctax - calcX._ospctax)
+
+	calcY_behavior = update_income(income_effect, calcY_behavior)
+
 	return calcY_behavior
+
+
+
+
 
