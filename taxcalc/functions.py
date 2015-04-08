e09e157fab3b40bf1b6e777a6a65ca6372505614
Matt Jensen <matt.jensen@aei.org>
2015-04-08 10:18:35 -0400

    parameterize StateLocalTax_HC

--- taxcalc/functions.py
+++ taxcalc/functions.py
@@ -106,12 +106,12 @@ def AGI(   _ymod1, c02500, c02700, e02615, c02900, e00100, e02500, XTOT,
     return (c02650, c00100, _agierr, _posagi, _ywossbe, _ywossbc, _prexmp, c04600)
 
 @iterate_jit(parameters=["puf", "ID_ps","ID_Medical_frt", "ID_Casualty_frt", "ID_Miscellaneous_frt",
-                         "ID_Charity_crt_Cash","ID_Charity_crt_Asset", "ID_prt", "ID_crt"], nopython=True, puf=True)
+                         "ID_Charity_crt_Cash","ID_Charity_crt_Asset", "ID_prt", "ID_crt", "ID_StateLocalTax_HC"], nopython=True, puf=True)
 def ItemDed(_posagi, e17500, e18400, e18425, e18450, e18500, e18800, e18900,
                  e20500, e20400, e19200, e20550, e20600, e20950, e19500, e19570,
                  e19400, e19550, e19800, e20100, e20200, e20900, e21000, e21010,
                  MARS, _sep, c00100, ID_ps,ID_Medical_frt, ID_Casualty_frt, ID_Miscellaneous_frt,
-                 ID_Charity_crt_Cash, ID_Charity_crt_Asset, ID_prt, ID_crt, puf):
+                 ID_Charity_crt_Cash, ID_Charity_crt_Asset, ID_prt, ID_crt, ID_StateLocalTax_HC, puf):
     """
     WARNING: Any additional keyword args, such as 'puf=True' here, must be passed
     to the function at the END of the argument list. If you stick the argument
@@ -158,7 +158,7 @@ def ItemDed(_posagi, e17500, e18400, e18425, e18450, e18500, e18800, e18900,
     # temporary fix!??
 
     # Gross Itemized Deductions #
-    c21060 = (e20900 + c17000 + c18300 + c19200 + c19700
+    c21060 = (e20900 + c17000 + (1-ID_StateLocalTax_HC)*c18300 + c19200 + c19700
               + c20500 + c20800 + e21000 + e21010)
 
     _phase2_i = ID_ps[MARS-1]
@@ -576,7 +576,7 @@ def TaxGains(e00650, c04800, e01000, c23650, e23250, e01100, e58990,
 
 @iterate_jit(parameters=["AMT_tthd", "II_brk6", "II_brk2", "AMT_Child_em", "cgrate1", 
                          "cgrate2", "AMT_em_ps", "AMT_em_pe", "KT_c_Age", "AMT_thd_MarriedS", 
-                         "AMT_em", "AMT_prt","AMT_trt1", "AMT_trt2", "puf"],
+                         "AMT_em", "AMT_prt","AMT_trt1", "AMT_trt2", "ID_StateLocalTax_HC", "puf"],
              nopython=True, puf=True)
 def AMTI(       c60000, _exact, e60290, _posagi, e07300, x60260, c24517,
                 e60300, e60860, e60100, e60840, e60630, e60550,
@@ -592,14 +592,14 @@ def AMTI(       c60000, _exact, e60290, _posagi, e07300, x60260, c24517,
                 cgrate2, AMT_em_ps, AMT_em_pe, x62720, e00700, c24516, 
                 c24520, c04800, e10105, c05700, e05800, e05100, e09600, 
                 KT_c_Age, x62740, e62900, AMT_thd_MarriedS, _earned, e62600, AMT_em,
-                AMT_prt, AMT_trt1, AMT_trt2, _cmbtp_itemizer, _cmbtp_standard, puf):
+                AMT_prt, AMT_trt1, AMT_trt2, _cmbtp_itemizer, _cmbtp_standard, ID_StateLocalTax_HC, puf):
 
     c62720 = c24517 + x62720
     c60260 = e00700 + x60260
     ## QUESTION: c63100 variable is reassigned below before use, is this a BUG?
     c63100 = max(0., _taxbc - e07300)
     c60200 = min(c17000, AMT_prt * _posagi)
-    c60240 = c18300 + x60240
+    c60240 = (1-ID_StateLocalTax_HC)*c18300 + x60240
     c60220 = c20800 + x60220
     c60130 = c21040 + x60130
     c62730 = e24515 + x62730 
@@ -636,7 +636,7 @@ def AMTI(       c60000, _exact, e60290, _posagi, e07300, x60260, c24517,
 
 
     if (puf == True and ((_standard == 0 or (_exact == 1 and e04470 > 0)))):
-        c62100 = (c00100 - c04470 + min(c17000, 0.025 * max(0., c00100)) + _sit
+        c62100 = (c00100 - c04470 + min(c17000, 0.025 * max(0., c00100)) + (1-ID_StateLocalTax_HC)*_sit
                + e18500 - c60260 + c20800 - c21040 ) 
         # c62100 += _cmbtp
 
