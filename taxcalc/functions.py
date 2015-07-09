8668ee5a15d4132056e76ef6cf87f62a433f2e20
Maria Messick <mmessick21@gmail.com>
2015-07-09 16:47:17 -0400

    changed computation of c62100

--- taxcalc/functions.py
+++ taxcalc/functions.py
@@ -864,8 +864,9 @@ def AMTI(   c60000, _exact, e60290, _posagi, e07300, x60260, c24517,
 
 
     if (puf == True and ((_standard == 0 or (_exact == 1 and e04470 > 0)))):
-        c62100 = (c00100 - c04470 + min(c17000, 0.025 * max(0., c00100)) + 
-            (1-ID_StateLocalTax_HC)*_sit + e18500 - c60260 + c20800 - c21040 )
+        c62100 = (c00100 - c04470 + max(0., min(c17000, 0.025 * c00100)) +
+                 (1-ID_StateLocalTax_HC)*_sit + c18300 - c60260 + c20800
+                  - c21040 + c60130)
         c62100 += _cmbtp
 
 
