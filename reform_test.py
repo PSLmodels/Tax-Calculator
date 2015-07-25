01bb7899e5e4288e10cae4b48617be8c6bdfa9a5
Martin Holmer <martin.holmer@gmail.com>
2015-07-25 19:00:23 -0400

    Revise reform_test.py to have only one-dimensional plus two-dimensional parameters in reform and to check two-dimensional parameter reform results.

--- reform_test.py
+++ reform_test.py
@@ -15,30 +15,6 @@ def main():
     """
     Test implementation of multi-year reform.
     """
-    # specify complexity of reform assumed in the test
-    reform_includes_2dim_parameters = False
-
-    # summarize test results
-    #
-    # If reform_includes_2dim_parameters == False, then
-    #   test produces NO ASSERT ERRORS
-    #
-    # If reform_includes_2dim_parameters == True, then
-    #   test produces the following Python error:
-    #   --------------------------------------------------------------------
-    #   tax-calculator$ python reform_test.py
-    #   Traceback (most recent call last):
-    #     File "reform_test.py", line 177, in <module>
-    #       exit(main())
-    #     File "reform_test.py", line 100, in main
-    #       ppo.update(year_provisions)
-    #     File "/Users/mrh/work/OSPC/tax-calculator/taxcalc/parameters.py",
-    #          line 125, in update
-    #       cur_val[offset:] = nval[num_years_to_skip:]
-    #   ValueError: could not broadcast input array from shape (7) into
-    #               shape (7,4)
-    #   --------------------------------------------------------------------
-
     # specify dimensions of policy Parameters object
     syr = 2013
     nyrs = 10
@@ -60,6 +36,13 @@ def main():
                            [40400, 41050]),
                                     inflate=True, inflation_rates=iratelist,
                                     num_years=nyrs))
+    assert_array_equal(getattr(ppo, '_EITC_c'),
+                       expand_array(np.array( #pylint: disable=no-member
+                           [[487, 3250, 5372, 6044],
+                            [496, 3305, 5460, 6143],
+                            [503, 3359, 5548, 6242]]),
+                                    inflate=True, inflation_rates=iratelist,
+                                    num_years=nyrs))
     assert_array_equal(getattr(ppo, '_II_em'),
                        expand_array(np.array( #pylint: disable=no-member
                            [3900, 3950, 4000]),
@@ -70,53 +53,27 @@ def main():
                            [113700, 117000, 118500]),
                                     inflate=True, inflation_rates=iratelist,
                                     num_years=nyrs))
-    assert_array_equal(getattr(ppo, '_EITC_c'),
-                       expand_array(np.array( #pylint: disable=no-member
-                           [[487, 3250, 5372, 6044],
-                            [496, 3305, 5460, 6143],
-                            [503, 3359, 5548, 6242]]),
-                                    inflate=True, inflation_rates=iratelist,
-                                    num_years=nyrs))
 
     # specify multi-year reform using a dictionary of year_provisions dicts
-    if reform_includes_2dim_parameters:
-        reform = {
-            2015: {
-                '_AMT_thd_MarriedS': [60000]
-            },
-            2016: {
-                '_EITC_c': [900, 5000, 8000, 9000],
-                '_II_em': [7000],
-                '_SS_Earnings_c': [300000]
-            },
-            2017: {
-                '_AMT_thd_MarriedS': [80000],
-                '_SS_Earnings_c': [500000]
-            },
-            2019: {
-                '_EITC_c': [1200, 7000, 10000, 12000],
-                '_II_em': [9000],
-                '_SS_Earnings_c': [700000]
-            }
+    reform = {
+        2015: {
+            '_AMT_thd_MarriedS': [60000]
+        },
+        2016: {
+            '_EITC_c': [[900, 5000, 8000, 9000]],
+            '_II_em': [7000],
+            '_SS_Earnings_c': [300000]
+        },
+        2017: {
+            '_AMT_thd_MarriedS': [80000],
+            '_SS_Earnings_c': [500000], '_SS_Earnings_c_cpi': False
+        },
+        2019: {
+            '_EITC_c': [[1200, 7000, 10000, 12000]],
+            '_II_em': [9000],
+            '_SS_Earnings_c': [700000], '_SS_Earnings_c_cpi': True
         }
-    else: # if reform includes only 1D parameters
-        reform = {
-            2015: {
-                '_AMT_thd_MarriedS': [60000]
-            },
-            2016: {
-                '_II_em': [7000],
-                '_SS_Earnings_c': [300000]
-            },
-            2017: {
-                '_AMT_thd_MarriedS': [80000],
-                '_SS_Earnings_c': [500000], '_SS_Earnings_c_cpi': False
-            },
-            2019: {
-                '_II_em': [9000],
-                '_SS_Earnings_c': [700000], '_SS_Earnings_c_cpi': True
-            }
-        }
+    }
 
     # implement reform using Parameters class update method
     #   Note: apparently must pass update method a YEAR:PROVISIONS dictionary,
@@ -146,6 +103,7 @@ def main():
 
     # confirm that actual parameters have expected post-reform values
     check_amt_thd_marrieds(ppo, reform, ifactors)
+    check_eitc_c(ppo, reform, ifactors)
     check_ii_em(ppo, reform, ifactors)
     check_ss_earnings_c(ppo, reform, ifactors)
 
@@ -153,7 +111,7 @@ def main():
     return 0
 
 
-def check_amt_thd_marrieds(ppo, reform, ifactors):
+def check_amt_thd_marrieds(ppo, reform, ifactor):
     """
     Compare actual and expected _AMT_thd_MarriedS parameter values.
     """
@@ -165,27 +123,64 @@ def check_amt_thd_marrieds(ppo, reform, ifactors):
     assert actual[2014] == 41050
     e2015 = reform[2015]['_AMT_thd_MarriedS'][0]
     assert actual[2015] == e2015
-    e2016 = int(round(ifactors[2016] * actual[2015]))
+    e2016 = int(round(ifactor[2016] * actual[2015]))
     assert actual[2016] == e2016
     e2017 = reform[2017]['_AMT_thd_MarriedS'][0]
     assert actual[2017] == e2017
-    e2018 = int(round(ifactors[2018] * actual[2017]))
+    e2018 = int(round(ifactor[2018] * actual[2017]))
     assert actual[2018] == e2018
-    e2019 = int(round(ifactors[2019] * actual[2018]))
+    e2019 = int(round(ifactor[2019] * actual[2018]))
     assert actual[2019] == e2019
-    e2020 = int(round(ifactors[2020] * actual[2019]))
+    e2020 = int(round(ifactor[2020] * actual[2019]))
     assert actual[2020] == e2020
-    e2021 = int(round(ifactors[2021] * actual[2020]))
+    e2021 = int(round(ifactor[2021] * actual[2020]))
     absdiff = abs(actual[2021] - e2021)
     if absdiff <= 1:
         pass # close enough for government work
     else:
         assert actual[2021] == e2021
-    e2022 = int(round(ifactors[2022] * actual[2021]))
+    e2022 = int(round(ifactor[2022] * actual[2021]))
     assert actual[2022] == e2022
 
 
-def check_ii_em(ppo, reform, ifactors):
+def check_eitc_c(ppo, reform, ifactor):
+    """
+    Compare actual and expected _EITC_c parameter values.
+    """
+    actual = {}
+    arr = getattr(ppo, '_EITC_c')
+    alen = len(arr[0])
+    for i in range(0, ppo.budget_years):
+        actual[ppo.start_year + i] = arr[i]
+    assert_array_equal(actual[2013], [487, 3250, 5372, 6044])
+    assert_array_equal(actual[2014], [496, 3305, 5460, 6143])
+    assert_array_equal(actual[2015], [503, 3359, 5548, 6242])
+    e2016 = reform[2016]['_EITC_c'][0]
+    assert_array_equal(actual[2016], e2016)
+    e2017 = [int(round(ifactor[2017] * actual[2016][j]))
+             for j in range(0, alen)]
+    print '2017 ifactor: ', ifactor[2017]
+    print '2017 actual: ', actual[2017], ' expected: ', e2017
+    assert_array_equal(actual[2017], e2017)
+    e2018 = [int(round(ifactor[2018] * actual[2017][j]))
+             for j in range(0, alen)]
+    print '2018 ifactor: ', ifactor[2018]
+    print '2018 actual: ', actual[2018], ' expected: ', e2018
+    assert_array_equal(actual[2018], e2018)
+    e2019 = reform[2019]['_EITC_c'][0]
+    assert_array_equal(actual[2019], e2019)
+    e2020 = [int(round(ifactor[2020] * actual[2019][j]))
+             for j in range(0, alen)]
+    assert_array_equal(actual[2020], e2020)
+    e2021 = [int(round(ifactor[2021] * actual[2020][j]))
+             for j in range(0, alen)]
+    assert_array_equal(actual[2021], e2021)
+    e2022 = [int(round(ifactor[2022] * actual[2021][j]))
+             for j in range(0, alen)]
+    assert_array_equal(actual[2022], e2022)
+
+
+def check_ii_em(ppo, reform, ifactor):
     """
     Compare actual and expected _II_em parameter values.
     """
@@ -198,21 +193,21 @@ def check_ii_em(ppo, reform, ifactors):
     assert actual[2015] == 4000
     e2016 = reform[2016]['_II_em'][0]
     assert actual[2016] == e2016
-    e2017 = int(round(ifactors[2017] * actual[2016]))
+    e2017 = int(round(ifactor[2017] * actual[2016]))
     assert actual[2017] == e2017
-    e2018 = int(round(ifactors[2018] * actual[2017]))
+    e2018 = int(round(ifactor[2018] * actual[2017]))
     assert actual[2018] == e2018
     e2019 = reform[2019]['_II_em'][0]
     assert actual[2019] == e2019
-    e2020 = int(round(ifactors[2020] * actual[2019]))
+    e2020 = int(round(ifactor[2020] * actual[2019]))
     assert actual[2020] == e2020
-    e2021 = int(round(ifactors[2021] * actual[2020]))
+    e2021 = int(round(ifactor[2021] * actual[2020]))
     assert actual[2021] == e2021
-    e2022 = int(round(ifactors[2022] * actual[2021]))
+    e2022 = int(round(ifactor[2022] * actual[2021]))
     assert actual[2022] == e2022
 
 
-def check_ss_earnings_c(ppo, reform, ifactors):
+def check_ss_earnings_c(ppo, reform, ifactor):
     """
     Compare actual and expected _SS_Earnings_c parameter values.
     """
@@ -231,11 +226,11 @@ def check_ss_earnings_c(ppo, reform, ifactors):
     assert actual[2018] == e2018
     e2019 = reform[2019]['_SS_Earnings_c'][0]
     assert actual[2019] == e2019
-    e2020 = int(round(ifactors[2020] * actual[2019])) # indexing after 2019
+    e2020 = int(round(ifactor[2020] * actual[2019])) # indexing after 2019
     assert actual[2020] == e2020
-    e2021 = int(round(ifactors[2021] * actual[2020]))
+    e2021 = int(round(ifactor[2021] * actual[2020]))
     assert actual[2021] == e2021
-    e2022 = int(round(ifactors[2022] * actual[2021]))
+    e2022 = int(round(ifactor[2022] * actual[2021]))
     absdiff = abs(actual[2022] - e2022)
     if absdiff <= 1:
         pass # close enough for government work
