5472ba6612f48175ecd9795a789380214a9101ae
talumbau <tj.alumbaugh@continuum.io>
2015-07-30 18:49:29 -0500

    remove debug statement

--- taxcalc/calculate.py
+++ taxcalc/calculate.py
@@ -102,10 +102,6 @@ class Calculator(object):
             print("Your data have beeen extrapolated to "
                   + str(self._records.current_year) + ".")
 
-        if self._params.current_year != self._records.current_year:
-            import pdb;pdb.set_trace()
-            print "uh oh"
-
         assert self._params.current_year == self._records.current_year
 
     @property
