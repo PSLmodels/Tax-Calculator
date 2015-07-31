67ed999289edf7c55e44269c7957534b45aaf3fa
Matt Jensen <matt.jensen@aei.org>
2015-07-31 11:16:34 -0400

    Revert "experimenting to get travisCI to pass - imported tests in taxcalc/__init__.py"
    This reverts commit 7e557a04dfe56bd77d468b7f89de96679f07aca6.

--- taxcalc/__init__.py
+++ taxcalc/__init__.py
@@ -4,7 +4,6 @@ from .behavior import *
 from .records import *
 from .utils import *
 from .decorators import *
-from tests import *
 
 from ._version import get_versions
 __version__ = get_versions()['version']
