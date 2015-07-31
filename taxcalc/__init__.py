7e557a04dfe56bd77d468b7f89de96679f07aca6
Matt Jensen <matt.jensen@aei.org>
2015-07-31 11:12:43 -0400

    experimenting to get travisCI to pass - imported tests in taxcalc/__init__.py

--- taxcalc/__init__.py
+++ taxcalc/__init__.py
@@ -4,6 +4,7 @@ from .behavior import *
 from .records import *
 from .utils import *
 from .decorators import *
+from tests import *
 
 from ._version import get_versions
 __version__ = get_versions()['version']
