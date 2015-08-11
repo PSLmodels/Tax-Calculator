bd45de50e1af0e5ddc1e486e6a297aa121ce2d26
Martin Holmer <martin.holmer@gmail.com>
2015-08-11 19:56:22 -0400

    Revise internal logic of several tests in test_parameters.py.
    These changes were required by the new signature of the Parameters
    class __init__ function.

--- taxcalc/tests/test_parameters.py
+++ taxcalc/tests/test_parameters.py
@@ -42,70 +42,74 @@ def test_create_parameters():
 
 
 def test_constant_inflation_rate_without_reform():
-    irate = 0.08
     syr = 2013
-    p = Parameters(start_year=syr, budget_years=10, inflation_rate=irate)
-    assert p._II_em[2013 - syr] == 3900
+    nyrs = 10
+    irate = 0.08
+    irates = {(syr + i): irate for i in range(0, nyrs)}
+    ppo = Parameters(start_year=syr, num_years=nyrs, inflation_rates=irates)
+    assert ppo._II_em[2013 - syr] == 3900
     # no reform
-    # check implied inflation rate at end of budget years that end in 2022
-    grate = float(p._II_em[2022 - syr]) / float(p._II_em[2021 - syr]) - 1.0
+    # check implied inflation rate at end of years
+    grate = float(ppo._II_em[2022 - syr]) / float(ppo._II_em[2021 - syr]) - 1.0
     assert round(grate, 3) == round(irate, 3)
 
 
 def test_constant_inflation_rate_with_reform():
-    irate = 0.08
     syr = 2013
-    p = Parameters(start_year=syr, budget_years=10, inflation_rate=irate)
+    nyrs = 10
+    irate = 0.08
+    irates = {(syr + i): irate for i in range(0, nyrs)}
+    ppo = Parameters(start_year=syr, num_years=nyrs, inflation_rates=irates)
     # implement reform in 2021 which is the year before the last year = 2022
     for yr in range(0, 8):
-        p.increment_year()
-    assert p.current_year == 2021
+        ppo.increment_year()
+    assert ppo.current_year == 2021
     reform = {2021: {"_II_em": [20000]}}
-    p.update(reform)
+    ppo.update(reform)
     # check implied inflation rate just before reform
-    grate = float(p._II_em[2020 - syr]) / float(p._II_em[2019 - syr]) - 1.0
+    grate = float(ppo._II_em[2020 - syr]) / float(ppo._II_em[2019 - syr]) - 1.0
     assert round(grate, 3) == round(irate, 3)
     # check implied inflation rate just after reform
-    grate = float(p._II_em[2022 - syr]) / float(p._II_em[2021 - syr]) - 1.0
+    grate = float(ppo._II_em[2022 - syr]) / float(ppo._II_em[2021 - syr]) - 1.0
     assert round(grate, 3) == round(irate, 3)
 
 
 def test_variable_inflation_rate_without_reform():
-    irates = {2013: 0.04, 2014: 0.04, 2015: 0.04, 2016: 0.04, 2017: 0.04,
-              2018: 0.04, 2019: 0.04, 2020: 0.04, 2021: 0.04, 2022: 0.08}
     syr = 2013
-    p = Parameters(start_year=syr, budget_years=10, inflation_rates=irates)
-    assert p._II_em[2013 - syr] == 3900
+    irates = {2013: 0.04, 2014: 0.04, 2015: 0.04, 2016: 0.04, 2017: 0.04,
+              2018: 0.04, 2019: 0.04, 2020: 0.04, 2021: 0.08, 2022: 0.08}
+    ppo = Parameters(start_year=syr, num_years=10, inflation_rates=irates)
+    assert ppo._II_em[2013 - syr] == 3900
     # no reform
     # check implied inflation rate between 2020 and 2021
-    grate = float(p._II_em[2021 - syr]) / float(p._II_em[2020 - syr]) - 1.0
+    grate = float(ppo._II_em[2021 - syr]) / float(ppo._II_em[2020 - syr]) - 1.0
     assert round(grate, 3) == round(0.04, 3)
     # check implied inflation rate between 2021 and 2022
-    grate = float(p._II_em[2022 - syr]) / float(p._II_em[2021 - syr]) - 1.0
-    assert round(grate, 3) == round(0.04, 3)
+    grate = float(ppo._II_em[2022 - syr]) / float(ppo._II_em[2021 - syr]) - 1.0
+    assert round(grate, 3) == round(0.08, 3)
 
 
 def test_variable_inflation_rate_with_reform():
-    irates = {2013: 0.04, 2014: 0.04, 2015: 0.04, 2016: 0.04, 2017: 0.04,
-              2018: 0.04, 2019: 0.04, 2020: 0.04, 2021: 0.04, 2022: 0.08}
     syr = 2013
-    p = Parameters(start_year=syr, budget_years=10, inflation_rates=irates)
-    assert p._II_em[2013 - syr] == 3900
+    irates = {2013: 0.04, 2014: 0.04, 2015: 0.04, 2016: 0.04, 2017: 0.04,
+              2018: 0.04, 2019: 0.04, 2020: 0.04, 2021: 0.08, 2022: 0.08}
+    ppo = Parameters(start_year=syr, num_years=10, inflation_rates=irates)
+    assert ppo._II_em[2013 - syr] == 3900
     # implement reform in 2020 which is two years before the last year, 2022
     for yr in range(0, 2020 - syr):
-        p.increment_year()
-    assert p.current_year == 2020
+        ppo.increment_year()
+    assert ppo.current_year == 2020
     reform = {2020: {"_II_em": [20000]}}
-    p.update(reform)
+    ppo.update(reform)
     # check implied inflation rate between 2018 and 2019 (before the reform)
-    grate = float(p._II_em[2019 - syr]) / float(p._II_em[2018 - syr]) - 1.0
+    grate = float(ppo._II_em[2019 - syr]) / float(ppo._II_em[2018 - syr]) - 1.0
     assert round(grate, 3) == round(0.04, 3)
-    # check implied inflation rate between 2020 and 2021 (after then reform)
-    grate = float(p._II_em[2021 - syr]) / float(p._II_em[2020 - syr]) - 1.0
+    # check implied inflation rate between 2020 and 2021 (after the reform)
+    grate = float(ppo._II_em[2021 - syr]) / float(ppo._II_em[2020 - syr]) - 1.0
     assert round(grate, 3) == round(0.04, 3)
-    # check implied inflation rate between 2021 and 2022 (after then reform)
-    grate = float(p._II_em[2022 - syr]) / float(p._II_em[2021 - syr]) - 1.0
-    assert round(grate, 3) == round(0.04, 3)
+    # check implied inflation rate between 2021 and 2022 (after the reform)
+    grate = float(ppo._II_em[2022 - syr]) / float(ppo._II_em[2021 - syr]) - 1.0
+    assert round(grate, 3) == round(0.08, 3)
 
 
 def test_multi_year_reform():
@@ -125,7 +129,7 @@ def test_multi_year_reform():
     iratelist = [irates[syr + i] for i in range(0, nyrs)]
 
     # instantiate policy Parameters object
-    ppo = Parameters(start_year=syr, budget_years=nyrs, inflation_rates=irates)
+    ppo = Parameters(start_year=syr, num_years=nyrs, inflation_rates=irates)
 
     # confirm that parameters have current-law values
     assert_array_equal(getattr(ppo, '_AMT_thd_MarriedS'),
@@ -205,7 +209,7 @@ def check_amt_thd_marrieds(ppo, reform, ifactor):
     """
     actual = {}
     arr = getattr(ppo, '_AMT_thd_MarriedS')
-    for i in range(0, ppo.budget_years):
+    for i in range(0, ppo.num_years):
         actual[ppo.start_year + i] = arr[i]
     assert actual[2013] == 40400
     assert actual[2014] == 41050
@@ -243,7 +247,7 @@ def check_eitc_c(ppo, reform, ifactor):
     actual = {}
     arr = getattr(ppo, '_EITC_c')
     alen = len(arr[0])
-    for i in range(0, ppo.budget_years):
+    for i in range(0, ppo.num_years):
         actual[ppo.start_year + i] = arr[i]
     assert_array_equal(actual[2013], [487, 3250, 5372, 6044])
     assert_array_equal(actual[2014], [496, 3305, 5460, 6143])
@@ -276,7 +280,7 @@ def check_ii_em(ppo, reform, ifactor):
     """
     actual = {}
     arr = getattr(ppo, '_II_em')
-    for i in range(0, ppo.budget_years):
+    for i in range(0, ppo.num_years):
         actual[ppo.start_year + i] = arr[i]
     assert actual[2013] == 3900
     assert actual[2014] == 3950
@@ -304,7 +308,7 @@ def check_ss_earnings_c(ppo, reform, ifactor):
     """
     actual = {}
     arr = getattr(ppo, '_SS_Earnings_c')
-    for i in range(0, ppo.budget_years):
+    for i in range(0, ppo.num_years):
         actual[ppo.start_year + i] = arr[i]
     assert actual[2013] == 113700
     assert actual[2014] == 117000
