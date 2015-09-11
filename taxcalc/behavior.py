da3b9f874f2d5445d0d771c64410c008cd2596df
Matt Jensen <matt.jensen@aei.org>
2015-09-10 21:55:58 -0400

    cleanup behavior()--everything running

--- taxcalc/behavior.py
+++ taxcalc/behavior.py
@@ -3,32 +3,32 @@ import copy
 
 
 def update_income(behavioral_effect, calcY):
-    delta_inc = np.where(calcY.c00100 > 0, behavioral_effect, 0)
+    delta_inc = np.where(calcY.records.c00100 > 0, behavioral_effect, 0)
 
     # Attribute the behavioral effects across itemized deductions,
     # wages, and other income.
 
-    _itemized = np.where(calcY.c04470 < calcY._standard,
-                         0, calcY.c04470)
+    _itemized = np.where(calcY.records.c04470 < calcY.records._standard,
+                         0, calcY.records.c04470)
     # TODO, verify that this is needed.
 
-    delta_wages = (delta_inc * calcY.e00200 /
-                   (calcY.c00100 + _itemized + .001))
+    delta_wages = (delta_inc * calcY.records.e00200 /
+                   (calcY.records.c00100 + _itemized + .001))
 
-    other_inc = calcY.c00100 - calcY.e00200
+    other_inc = calcY.records.c00100 - calcY.records.e00200
 
     delta_other_inc = (delta_inc * other_inc /
-                       (calcY.c00100 + _itemized + .001))
+                       (calcY.records.c00100 + _itemized + .001))
 
     delta_itemized = (delta_inc * _itemized /
-                      (calcY.c00100 + _itemized + .001))
+                      (calcY.records.c00100 + _itemized + .001))
 
-    calcY.e00200 = calcY.e00200 + delta_wages
+    calcY.records.e00200 = calcY.records.e00200 + delta_wages
 
-    calcY.e00300 = calcY.e00300 + delta_other_inc
+    calcY.records.e00300 = calcY.records.e00300 + delta_other_inc
 
-    calcY.e19570 = np.where(_itemized > 0,
-                            calcY.e19570 + delta_itemized, 0)
+    calcY.records.e19570 = np.where(_itemized > 0,
+                                    calcY.records.e19570 + delta_itemized, 0)
     # TODO, we should create a behavioral modification
     # variable instead of using e19570
 
@@ -56,11 +56,11 @@ def behavior(calcX, calcY, elast_wrt_atr=0.4, inc_effect=0.15,
 
     # Calculate the magnitude of the substitution and income effects.
     substitution_effect = (elast_wrt_atr * pct_diff_atr *
-                           (calcX._ospctax))
+                           (calcX.records._ospctax))
 
     calcY_behavior = update_income(substitution_effect, calcY_behavior)
 
-    income_effect = inc_effect * (calcY_behavior._ospctax - calcX._ospctax)
+    income_effect = inc_effect * (calcY_behavior.records._ospctax - calcX.records._ospctax)
 
     calcY_behavior = update_income(income_effect, calcY_behavior)
 
