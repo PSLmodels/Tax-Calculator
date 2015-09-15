3af0c54156cad06df6ad468915bf1395c0694e95
Xiyu <xiyuw123@gmail.com>
2015-09-15 12:29:07 -0400

    Travis CI edits

--- calc_to_ascii.py
+++ calc_to_ascii.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+
 def calc_to_ascii(calc, ascii_results=""):
     '''
     Takes in calc object to print relevant records into ascii form
@@ -9,19 +10,20 @@ def calc_to_ascii(calc, ascii_results=""):
         print("no output file name given!")
         exit(0)
 
-    #list of integers corresponding to the number(s) of the row(s) in the
-    #csv file, only rows in list will be recorded in final output
-    #if left as [], results in entire file being converted to ascii
-    #put in order from smallest to largest, for example:
-    #recids = [33180, 64023, 68020, 74700, 84723, 98001, 107039, 107298, 108820]
+    # list of integers corresponding to the number(s) of the row(s) in the
+    # csv file, only rows in list will be recorded in final output
+    # if left as [], results in entire file being converted to ascii
+    # put in order from smallest to largest, for example:
+    # recids = [33180, 64023, 68020, 74700, 84723, 98001, 107039, 107298, 
+    #           108820]
     recids = [1, 4, 5]
     
-    #Number of characters in each column, must be whole nonnegative integer
+    # Number of characters in each column, must be whole nonnegative integer
     col_size = 15
 
-    #creates empty dataframe
+    # creates empty dataframe
     df = pd.DataFrame()
-    #gets dimensions of attributes that can be converted into a dataframe
+    # gets dimensions of attributes that can be converted into a dataframe
     rshape = calc.records.e00100.shape
 
     for attr in dir(calc.records):
