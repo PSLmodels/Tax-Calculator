1b5953cfaee552c8f4687cea1c48f823664fe64f
Martin Holmer <martin.holmer@gmail.com>
2015-09-24 14:10:54 -0400

    Revise test_behavior.py to use Parameters class correctly.
    Also, eliminate many (but not all) pylint warnings.

--- taxcalc/tests/test_behavior.py
+++ taxcalc/tests/test_behavior.py
@@ -1,52 +1,40 @@
 import os
 import sys
-import json
 CUR_PATH = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(os.path.join(CUR_PATH, "../../"))
-import numpy as np
-from numpy.testing import assert_array_equal
 import pandas as pd
-import tempfile
-import pytest
-from taxcalc import *
-import copy
+from taxcalc import Parameters, Records, Calculator, behavior
+
 
 WEIGHTS_FILENAME = "../../WEIGHTS_testing.csv"
-weights_path = os.path.join(CUR_PATH, WEIGHTS_FILENAME)
-weights = pd.read_csv(weights_path)
+WEIGHTS_PATH = os.path.join(CUR_PATH, WEIGHTS_FILENAME)
+WEIGHTS = pd.read_csv(WEIGHTS_PATH)
 
-all_cols = set()
-tax_dta_path = os.path.join(CUR_PATH, "../../tax_all1991_puf.gz")
-tax_dta = pd.read_csv(tax_dta_path, compression='gzip')
-
-# Fix-up. MIdR needs to be type int64 to match PUF
-tax_dta['midr'] = tax_dta['midr'].astype('int64')
-tax_dta['s006'] = np.arange(0, len(tax_dta['s006']))
-
-irates = {1991: 0.015, 1992: 0.020, 1993: 0.022, 1994: 0.020, 1995: 0.021,
-          1996: 0.022, 1997: 0.023, 1998: 0.024, 1999: 0.024, 2000: 0.024,
-          2001: 0.024, 2002: 0.024}
+TAX_DTA_PATH = os.path.join(CUR_PATH, "../../tax_all1991_puf.gz")
+TAX_DTA = pd.read_csv(TAX_DTA_PATH, compression='gzip')
+# data fix-up: midr needs to be type int64 to match PUF
+TAX_DTA['midr'] = TAX_DTA['midr'].astype('int64')
 
 
 def test_make_behavioral_Calculator():
-    # Create a Records and Params object
-    records_x = Records(tax_dta)
-    records_y = Records(tax_dta)
-    params_x = Parameters(start_year=1991, inflation_rates=irates)
-    params_y = Parameters(start_year=1991, inflation_rates=irates)
-    # Create two Calculators
-    calcX = Calculator(params_x, records_x)
-    calcY = Calculator(params_y, records_y)
-    # Implement a plan Y reform
-    reform = {1991: {"_II_rt7": [0.496]}}
+    # create a Records and Params object
+    records_x = Records(data=TAX_DTA, weights=WEIGHTS, start_year=2009)
+    records_y = Records(data=TAX_DTA, weights=WEIGHTS, start_year=2009)
+    params_x = Parameters()
+    params_y = Parameters()
+    # create two Calculators
+    calc_x = Calculator(params=params_x, records=records_x)
+    calc_y = Calculator(params=params_y, records=records_y)
+    # implement a plan Y reform
+    reform = {2013: {"_II_rt7": [0.496]}}
     params_y.implement_reform(reform)
     # Create behavioral calculators and vary both kwargs.
-    calcY_behavior1 = behavior(calcX, calcY,
-                               elast_wrt_atr=0.4, inc_effect=0.15)
-    calcY_behavior2 = behavior(calcX, calcY,
-                               elast_wrt_atr=0.5, inc_effect=0.15)
-    calcY_behavior3 = behavior(calcX, calcY,
-                               elast_wrt_atr=0.4, inc_effect=0)
-    assert (calcY_behavior1.records._ospctax.sum() !=
-            calcY_behavior2.records._ospctax.sum() !=
-            calcY_behavior3.records._ospctax.sum())
+    calc_y_behavior1 = behavior(calc_x, calc_y,
+                                elast_wrt_atr=0.4, inc_effect=0.15)
+    calc_y_behavior2 = behavior(calc_x, calc_y,
+                                elast_wrt_atr=0.5, inc_effect=0.15)
+    calc_y_behavior3 = behavior(calc_x, calc_y,
+                                elast_wrt_atr=0.4, inc_effect=0)
+    assert (calc_y_behavior1.records._ospctax.sum() !=
+            calc_y_behavior2.records._ospctax.sum() !=
+            calc_y_behavior3.records._ospctax.sum())
