344960d54f02ba630893363485c8fe968964b956
Amy-Xu <amy.xu@aei.org>
2015-10-09 12:06:46 -0400

    pep8

--- taxcalc/calculate.py
+++ taxcalc/calculate.py
@@ -49,8 +49,8 @@ class Calculator(object):
         if sync_years and self._records.current_year == Records.PUF_YEAR:
             print("You loaded data for " +
                   str(self._records.current_year) + '.')
-            
-            if self._records.current_year == 2009:		
+
+            if self._records.current_year == 2009:
                 self.records.extrapolate_2009_puf()
 
             while self._records.current_year < self._params.current_year:
