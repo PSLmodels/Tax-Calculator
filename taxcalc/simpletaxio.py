c528755eb6d79db89300551434935656ca5e9454
Martin Holmer <martin.holmer@gmail.com>
2015-10-15 14:08:00 -0400

    Change SimpleTaxIO use of revised Calculator mtr method.
    Now computes marginal tax rates for small increase in taxpayer
    earnings (the default on the Internet TAXSIM web page) and with
    wrt_expanded_income=False (as in current version of TAXSIM).

--- taxcalc/simpletaxio.py
+++ taxcalc/simpletaxio.py
@@ -93,7 +93,7 @@ class SimpleTaxIO(object):
                     self._output[lnum] = ovar
             if not no_marginal_tax_rates:
                 (mtr_fica, mtr_itax,
-                 _) = self._calc.mtr('e00200', wrt_expanded_income=True)
+                 _) = self._calc.mtr('e00200p', wrt_expanded_income=False)
                 for idx in range(0, self._calc.records.dim):
                     indyr = cr_taxyr[idx]
                     if indyr == calcyr:
