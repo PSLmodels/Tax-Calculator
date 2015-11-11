a6699d5d0cfcb99ba7c30aa0cbfae0527ea2bfce
Amy-Xu <amy.xu@aei.org>
2015-11-11 15:20:32 -0500

    added paramter for CTC reform

--- taxcalc/functions.py
+++ taxcalc/functions.py
@@ -1608,7 +1608,7 @@ def DEITC(c08795, c59660, c09200, c07100, c08800, c05800, _avail, _othertax):
 @iterate_jit(nopython=True)
 def OSPC_TAX(c09200, c59660, c11070, c10960, c11600, c10950, _eitc, e11580,
              e11450, e11500, e82040, e09900, e11400, e11300, e11200, e11100,
-             e11550, e09710, e09720, e10000, _fica):
+             e11550, e09710, e09720, e10000, _fica, CTC_additional):
 
     _refund = (c59660 + c11070 + c10960 + c10950 + c11600 + e11580 + e11450 +
                e11500)
@@ -1616,6 +1616,10 @@ def OSPC_TAX(c09200, c59660, c11070, c10960, c11600, c10950, _eitc, e11580,
     _ospctax = c09200 - _refund - e82040
 
     _combined = _ospctax + _fica
+
+    partially_refundable_CTC = min(_combined, CTC_additional)
+
+    _ospctax = _ospctax - partially_refundable_CTC
 
     _payments = (c59660 + c10950 + c10960 + c11070 + e10000 + e11550 + e11580 +
                  e11450)
