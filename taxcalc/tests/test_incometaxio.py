7d45e707a31753f16a8a45047abd97cef52e95c4
martinholmer <martin.holmer@gmail.com>
2016-01-22 21:21:02 -0500

    Add incometaxio test that used a reform.

--- taxcalc/tests/test_incometaxio.py
+++ taxcalc/tests/test_incometaxio.py
@@ -71,14 +71,69 @@ def rawinputfile():
             pass  # sometimes we can't remove a generated temporary file
 
 
-def test_3(rawinputfile):  # pylint: disable=redefined-outer-name
+REFORM_CONTENTS = """
+// Example of a reform suitable for use as an optional IncomeTaxIO reform file.
+// This JSON file can contain any number of trailing //-style comments, which
+// will be removed before the contents are converted from JSON to a dictionary.
+// The primary keys are policy parameters and secondary keys are years.
+// Both the primary and secondary key values must be enclosed in quotes (").
+// Boolean variables are specified as true or false (no quotes; all lowercase).
+{
+    "_AMT_tthd": // AMT taxinc threshold separating the two AMT tax brackets
+    {"2015": [200000],
+     "2017": [300000]
+    },
+    "_EITC_c": // maximum EITC amount by number of qualifying kids (0,1,2,3+)
+    {"2016": [[ 900, 5000,  8000,  9000]],
+     "2019": [[1200, 7000, 10000, 12000]]
+    },
+    "_II_em": // personal exemption amount (see indexing changes below)
+    {"2016": [6000],
+     "2018": [7500],
+     "2020": [9000]
+    },
+    "_II_em_cpi": // personal exemption amount indexing status
+    {"2016": false, // values in future years are same as this year value
+     "2018": true   // values in future years indexed with this year as base
+    },
+    "_SS_Earnings_c": // social security (OASDI) maximum taxable earnings
+    {"2016": [300000],
+     "2018": [500000],
+     "2020": [700000]
+    },
+    "_AMT_em_cpi": // AMT exemption amount indexing status
+    {"2017": false, // values in future years are same as this year value
+     "2020": true   // values in future years indexed with this year as base
+    }
+}
+"""
+
+
+@pytest.yield_fixture
+def reformfile():
+    """
+    Temporary reform file for IncomeTaxIO constructor.
+    """
+    rfile = tempfile.NamedTemporaryFile(mode='a', delete=False)
+    rfile.write(REFORM_CONTENTS)
+    rfile.close()
+    # must close and then yield for Windows platform
+    yield rfile
+    if os.path.isfile(rfile.name):
+        try:
+            os.remove(rfile.name)
+        except OSError:
+            pass  # sometimes we can't remove a generated temporary file
+
+
+def test_3(rawinputfile, reformfile):  # pylint: disable=redefined-outer-name
     """
     Test IncomeTaxIO calculate method with no output writing and with blowup.
     """
     taxyear = 2021
     inctax = IncomeTaxIO(input_filename=rawinputfile.name,
                          tax_year=taxyear,
-                         reform_filename=None,
-                         blowup_input_data=True)
+                         reform_filename=reformfile.name,
+                         blowup_input_data=False)
     inctax.calculate(write_output_file=False)
     assert inctax.tax_year() == taxyear
