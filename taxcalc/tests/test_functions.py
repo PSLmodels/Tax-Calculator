2e2ca390769c764df28d0129c8b685ebb6ef5c77
martinholmer <martin.holmer@gmail.com>
2016-01-27 11:59:20 -0500

    Add second filing unit to test_1 in new test_functions.py file.
    The second unit is an individual with both wage-and-salary and
    self-employment income, so that this situation is tested.  The
    expanded test_1 passes and the taxcalc/validation/tests continue to
    pass because simtax.py input contains no self-employment income.

--- taxcalc/tests/test_functions.py
+++ taxcalc/tests/test_functions.py
@@ -33,11 +33,13 @@ def test_1():
         }
     }
     funit = (
-        u'RECID,MARS,e00200,e00200p,e00900,e00900s\n'
-        u'1,    2,   200000, 200000,200000, 200000\n'
+        u'RECID,MARS,e00200,e00200p,e00900,e00900p,e00900s\n'
+        u'1,    2,   200000, 200000,200000,      0, 200000\n'
+        u'2,    1,   100000, 100000,200000, 200000,      0\n'
     )
+    # ==== Filing unit with RECID=1:
     # The expected AGI for this filing unit is $400,000 minus half of
-    # the  spouse's Self-Employment Tax (SET), which represents the
+    # the spouse's Self-Employment Tax (SET), which represents the
     # "employer share" of the SET.  The spouse pays OASDI SET on only
     # $100,000 of self-employment income, which implies a tax of
     # $12,400.  The spouse also pays HI SET on 0.9235 * 200000 * 0.029,
@@ -45,13 +47,26 @@ def test_1():
     # the sum of the two, which equals $17,756.30.
     # The SET AGI adjustment is one-half of that amount, which is $8,878.15.
     # So, filing unit's AGI is $400,000 less this, which is $391,121.85.
-    expected_agi = '391121.85'
+    expected_agi_1 = '391121.85'
+    # ==== Filing unit with RECID=2:
+    # The expected AGI for this filing unit is $300,000 minus half of
+    # the individual's Self-Employment Tax (SET), which represents the
+    # "employer share" of the SET.  The individual pays no OASDI SET
+    # because wage and salary income was already at the MTE.  So, the
+    # only SET the individual pays is for HI, which is a tax equal to
+    # 0.9235 * 200000 * 0.029, or $5,356.30.  One half of that amount
+    # is $2,678.15, which implies the AGI is $297,321.85.
+    expected_agi_2 = '297321.85'
     input_dataframe = pd.read_csv(StringIO(funit))
     inctax = IncomeTaxIO(input_data=input_dataframe,
                          tax_year=2015,
                          policy_reform=reform,
                          blowup_input_data=False)
     output = inctax.calculate()
-    output_list = output.split()
-    agi = output_list[agi_ovar_num - 1]
-    assert agi == expected_agi
+    output_lines_list = output.split('\n')
+    output_vars_list = output_lines_list[0].split()
+    agi = output_vars_list[agi_ovar_num - 1]
+    assert agi == expected_agi_1
+    output_vars_list = output_lines_list[1].split()
+    agi = output_vars_list[agi_ovar_num - 1]
+    assert agi == expected_agi_2
