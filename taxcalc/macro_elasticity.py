6617a858ac4588a35d7680242ed1fb79447b5c2c
Amy-Xu <amy.xu@aei.org>
2016-03-14 08:59:56 -0400

    changed 1-mtr to 1-amtr

--- taxcalc/macro_elasticity.py
+++ taxcalc/macro_elasticity.py
@@ -1,15 +1,15 @@
 import pandas as pd
 
-def percentage_change_gdp(calc1, calc2, elasticity=0.36):
+def percentage_change_gdp(calc1, calc2, elasticity=0.0):
     
     mtr_fica_x, mtr_iit_x, mtr_combined_x = calc1.mtr()
     mtr_fica_y, mtr_iit_y, mtr_combined_y = calc2.mtr()
     
-    after_tax_mtr_x = (((1 - mtr_combined_x) * calc1.records.c00100 *
+    after_tax_mtr_x = (1 - ((mtr_combined_x) * calc1.records.c00100 *
                         calc1.records.s006).sum()/
                         (calc1.records.c00100 * calc1.records.s006).sum())
 
-    after_tax_mtr_y = (((1 - mtr_combined_y) * calc2.records.c00100 *
+    after_tax_mtr_y = (1 - ((mtr_combined_y) * calc2.records.c00100 *
                         calc2.records.s006).sum()/
                         (calc2.records.c00100 * calc2.records.s006).sum())
 
