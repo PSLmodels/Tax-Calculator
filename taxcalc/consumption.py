da80b68ca7083cf5105c587b3cfe684c02a0842f
martinholmer <martin.holmer@gmail.com>
2016-06-13 16:12:46 -0400

    Initial incomplete version of Consumption class.

---
+++ taxcalc/consumption.py
@@ -0,0 +1,121 @@
+"""
+Tax-Calculator marginal Consumption class.
+"""
+# CODING-STYLE CHECKS:
+# pep8 --ignore=E402 consumption.py
+# pylint --disable=locally-disabled consumption.py
+# (when importing numpy, add "--extension-pkg-whitelist=numpy" pylint option)
+
+from .policy import Policy
+from .parameters import ParametersBase
+
+
+class Consumption(ParametersBase):
+    """
+    Constructor for marginal Consumption class.
+
+    Parameters
+    ----------
+    consumption_dict: dictionary of PARAM:DESCRIPTION pairs
+        dictionary of marginal propensity to consume (MPC) parameters;
+        if None, all MPC parameters are read from DEFAULTS_FILENAME file.
+
+    start_year: integer
+        first calendar year for MPC parameters.
+
+    num_years: integer
+        number of calendar years for which to specify MPC parameter
+        values beginning with start_year.
+
+    Raises
+    ------
+    ValueError:
+        if consumption_dict is neither None nor a dictionary.
+        if num_years is less than one.
+
+    Returns
+    -------
+    class instance: Consumption
+    """
+
+    JSON_START_YEAR = Policy.JSON_START_YEAR
+    DEFAULTS_FILENAME = 'consumption.json'
+    DEFAULT_NUM_YEARS = Policy.DEFAULT_NUM_YEARS
+
+    def __init__(self, consumption_dict=None,
+                 start_year=JSON_START_YEAR,
+                 num_years=DEFAULT_NUM_YEARS):
+        super(Consumption, self).__init__()
+        if consumption_dict is None:
+            self._vals = self._params_dict_from_json_file()
+        elif isinstance(consumption_dict, dict):
+            self._vals = consumption_dict
+        else:
+            raise ValueError('consumption_dict is not None or a dictionary')
+        if num_years < 1:
+            raise ValueError('num_years < 1 in Consumption ctor')
+        self.initialize(start_year, num_years)
+        if consumption_dict is not None:
+            self._validate_mpc_values()
+
+    def update_consumption(self, revisions):
+        """
+        Update consumption for given revisions, a dictionary consisting
+        of one or more year:modification dictionaries.
+        For example: {2014: {'_MPC_xxx': [0.2, 0.1]}}
+        Also checks for valid MPC parameter values in revisions dictionary.
+        NOTE: this method uses the specified revisions to update the
+              DEFAULT MPC parameter values, so use this method just once
+              rather than calling it sequentially in an attempt to update
+              MPC parameters in several steps.
+        """
+        precall_current_year = self.current_year
+        self.set_default_vals()
+        # specify revisions
+        revision_years = sorted(list(revisions.keys()))
+        for year in revision_years:
+            self.set_year(year)
+            # enforce valid MPC values in each revisons[year] dictionary
+            for mpc in revisions[year]:
+                for idx in range(len(revisions[year][mpc])):
+                    val = revisions[year][mpc][idx]
+                    if val < 0.0 or val > 1.0:
+                        msg = '{} is outside [0,1] range; value is {}'
+                        raise ValueError(msg.format(mpc, val))
+            # update valid MPC parameter values for year
+            self._update({year: revisions[year]})
+        self.set_year(precall_current_year)
+        self._validate_mpc_values()
+
+    def has_response(self):
+        """
+        Returns true if any MPC parameters are positive for current_year;
+        returns false if all MPC parameters are zero.
+        """
+        # pylint: disable=no-member
+        if self.MPC_xxx > 0.0:
+            return True
+        else:
+            return False
+
+    # ----- begin private methods of Consumption class -----
+
+    def _validate_mpc_values(self):
+        """
+        Check that each MPC parameter is in [0,1] range and that
+        sum of all MPC parameters in a year is no more than one.
+        """
+        for iyr in range(0, self.num_years):
+            sum_mpc = 0.0
+            for mpc in self._vals:
+                val = getattr(self, mpc)[iyr]
+                sum_mpc += val
+                if val < 0.0 or val > 1.0:
+                    msg = '{} is outside [0,1] range in year {}; value is {}'
+                    cyr = iyr + self.start_year
+                    raise ValueError(msg.format(mpc, cyr, val))
+            if sum_mpc > 1.0:
+                msg = 'sum of MPC = {} is greater than one for year {}'
+                raise ValueError(msg.format(sum_mpc, iyr + self.start_year))
+
+# end Consumption class
---
+++ taxcalc/tests/test_consumption.py
@@ -0,0 +1,73 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+import pytest
+CUR_PATH = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(os.path.join(CUR_PATH, '..', '..'))
+from taxcalc import Policy, Records, Calculator, Consumption
+
+# use 1991 PUF-like data to emulate current puf.csv, which is private
+TAXDATA_PATH = os.path.join(CUR_PATH, '..', 'altdata', 'puf91taxdata.csv.gz')
+TAXDATA = pd.read_csv(TAXDATA_PATH, compression='gzip')
+WEIGHTS_PATH = os.path.join(CUR_PATH, '..', 'altdata', 'puf91weights.csv.gz')
+WEIGHTS = pd.read_csv(WEIGHTS_PATH, compression='gzip')
+
+
+def test_incorrect_Consumption_instantiation():
+    with pytest.raises(ValueError):
+        consump = Consumption(consumption_dict=list())
+    with pytest.raises(ValueError):
+        consump = Consumption(num_years=0)
+
+
+def test_correct_but_not_recommended_Consumption_instantiation():
+    consump = Consumption(consumption_dict={})
+    assert consump
+
+
+def test_correct_update_consumption():
+    consump = Consumption(start_year=2013)
+    consump.update_consumption({2014: {'_MPC_xxx': [0.05]},
+                                2015: {'_MPC_xxx': [0.06]}})
+    should_be = np.full((Consumption.DEFAULT_NUM_YEARS,), 0.06)
+    should_be[0] = 0.0
+    should_be[1] = 0.05
+    assert np.allclose(consump._MPC_xxx, should_be, rtol=0.0)
+    assert np.allclose(consump._MPC_yyy,
+                       np.zeros((Consumption.DEFAULT_NUM_YEARS,)),
+                       rtol=0.0)
+    consump.set_year(2015)
+    assert consump.current_year == 2015
+    assert consump.MPC_xxx == 0.06
+    assert consump.MPC_yyy == 0.0
+
+
+def test_incorrect_update_consumption():
+    consump = Consumption()
+    with pytest.raises(ValueError):
+        consump.update_consumption({2016: {'_MPC_xxx': [-0.02]}})
+    with pytest.raises(ValueError):
+        consump.update_consumption({2017: {'_MPC_xxx': [1.02]}})
+    with pytest.raises(ValueError):
+        consump.update_consumption({2014: {'_MPC_xxx': [0.8],
+                                           'MPC_yyy': [0.3]}})
+
+
+def test_future_update_consumption():
+    consump = Consumption()
+    assert consump.current_year == consump.start_year
+    assert consump.has_response() is False
+    cyr = 2020
+    consump.set_year(cyr)
+    consump.update_consumption({cyr: {'_MPC_xxx': [0.01]}})
+    assert consump.current_year == cyr
+    assert consump.has_response() is True
+    consump.set_year(cyr - 1)
+    assert consump.has_response() is False
+
+
+def test_consumption_default_data():
+    paramdata = Consumption.default_data()
+    for param in paramdata:
+        assert paramdata[param] == [0.0]
