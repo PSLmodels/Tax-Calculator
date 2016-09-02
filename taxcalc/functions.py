999befb9ae56bcaa6bd8e23aab59795e3775123c
martinholmer <martin.holmer@gmail.com>
2016-09-02 08:46:15 -0400

    Fix SchR() function by making logic more like Schedule R.

--- taxcalc/functions.py
+++ taxcalc/functions.py
@@ -941,39 +941,39 @@ def SchR(age_head, age_spouse, MARS, c00100,
          c07200):
     """
     Calculate Schedule R credit for the elderly and the disabled, c07200
+
+    Note that all Schedule R policy parameters are not inflation indexed.
+
+    Note that all Schedule R policy parameters are hard-coded, and therefore,
+    are not able to be changed using Policy class parameters.
     """
     if age_head >= 65 or (MARS == 2 and age_spouse >= 65):
-        # calculate credit assuming nobody is disabled
-        # (note that all Schedule R policy parameters are hard-coded)
-        # Part I and first line in Part III
+        # calculate credit assuming nobody is disabled (so line12 = line10)
         if MARS == 2:
             if age_head >= 65 and age_spouse >= 65:
-                c28300 = 7500.
+                schr12 = 7500.
             else:
-                c28300 = 5000.
+                schr12 = 5000.
+            schr15 = 10000.
         elif MARS == 3:
-            c28300 = 3750.
+            schr12 = 3750.
+            schr15 = 5000.
         elif MARS == 1 or MARS == 4:
-            c28300 = 5000.
+            schr12 = 5000.
+            schr15 = 7500.
         else:
-            c28300 = 0.
-        # nontaxable OASDI benefit plus nontaxable pension benefits
-        c28400 = max(0., (e02400 - c02500) + (e01500 - e01700))
-        # one-half of adjusted AGI
-        if MARS == 2:
-            c28500 = max(0., c00100 - 10000.)
-        elif MARS == 3:
-            c28500 = max(0., c00100 - 5000.)
-        elif MARS == 1 or MARS == 4:
-            c28500 = max(0., c00100 - 7500.)
-        else:
-            c28500 = 0.
-        c28600 = 0.5 * c28500
-        # compute credit amount, c07200
-        c28700 = c28400 + c28600
-        c28800 = max(0., c28300 - c28700)
-        c07200 = min(0.15 * c28800,
-                     max(0., (c05800 - e07300 - c07180)))
+            schr12 = 0.
+            schr15 = 0.
+        schr13a = max(0., e02400 - c02500)
+        schr13b = max(0., e01500 - e01700)
+        schr13c = schr13a + schr13b
+        schr16 = max(0., c00100 - schr15)
+        schr17 = 0.5 * schr16
+        schr18 = schr13c + schr17
+        schr19 = max(0., schr12 - schr18)
+        schr20 = 0.15 * schr19
+        schr21 = max(0., (c05800 - e07300 - c07180))
+        c07200 = min(schr20, schr21)
     else:  # if not calculating Schedule R credit
         c07200 = 0.
     return c07200
