29ba59300ac65b3411053334ed866550de26e471
martinholmer <martin.holmer@gmail.com>
2016-09-09 19:01:25 -0400

    Initial completed version of validation/csv_in.py script.

--- taxcalc/validation/csv_in.py
+++ taxcalc/validation/csv_in.py
@@ -25,7 +25,7 @@ MAX_SIZE = 100000  # maximum size of sample to draw from puf.csv
 
 DEBUG = False  # True implies no variable randomization or record sampling
 
-TRACE = True  # True implies tracing output written to stdout
+TRACE = False  # True implies tracing output written to stdout
 
 # specify set of variables slated for removal from puf.csv
 DROP1_VARS = set(['e11070', 'e11550', 'FDED'])
@@ -46,93 +46,6 @@ else:
                      'age_head', 'age_spouse',
                      'XTOT', 'EIC', 'n24', 'f2441',
                      'f6251'])
-"""
-validation$ ~/work/OSPC/csvvars ~/work/OSPC/tax-calculator-data/puf.csv
- 1 age_head
- 2 age_spouse
- 3 DSI
- 4 EIC
- 5 FDED
- 6 FLPDYR
- 7 f2441
- 8 f6251
- 9 MARS
-10 MIDR
-11 n24
-12 XTOT
-13 e00200
-14 e00300
-15 e00400
-16 e00600
-17 e00650
-18 e00700
-19 e00800
-20 e00900
-21 e01100
-22 e01200
-23 e01400
-24 e01500
-25 e01700
-26 e02000
-27 e02100
-28 e02300
-29 e02400
-30 e03150
-31 e03210
-32 e03220
-33 e03230
-34 e03270
-35 e03240
-36 e03290
-37 e03300
-38 e03400
-39 e03500
-40 e07240
-41 e07260
-42 e07300
-43 e07400
-44 e07600
-45 p08000
-46 e09700
-47 e09800
-48 e09900
-49 e11550
-50 e11070
-51 e11200
-52 e11580
-53 e17500
-54 e18400
-55 e18500
-56 e19200
-57 e19800
-58 e20100
-59 e20400
-60 e20500
-61 p22250
-62 p23250
-63 e24515
-64 e24518
-65 p25470
-66 e26270
-67 e27200
-68 e32800
-69 e58990
-70 e62900
-71 p87482
-72 p87521
-73 e87530
-74 RECID
-75 s006
-76 filer
-77 cmbtp_standard
-78 cmbtp_itemizer
-79 e00200p
-80 e00200s
-81 e00900p
-82 e00900s
-83 e02100p
-84 e02100s
-"""
 
 ANNUAL_DRIFT = 0.03
 NORM_STD_DEV = 0.25
@@ -152,11 +65,6 @@ def randomize_data(xdf, taxyear, rnseed):
 
     rnseed: integer
         specifies random-number seed to use in the randomization.
-
-    Returns
-    -------
-    return_code: integer
-        where zero indicates success and nonzero indicates an error.
     """
     xdf['FLPDYR'] = taxyear
     num = xdf['FLPDYR'].size
@@ -168,14 +76,15 @@ def randomize_data(xdf, taxyear, rnseed):
         if varname in SKIP_VARS:
             num_skips += 1
             continue
-        # randomize varname amounts
+        # randomize nonzero variable amounts
         old = xdf[varname]
+        oldint = old.round(decimals=0)
+        oldint = oldint.astype(dtype=np.int64)
         rfactor = np.random.normal(loc=nmean, scale=nsdev, size=num)
-        addon = old * rfactor
-        raw = old + addon
-        raw = raw.round(decimals=0)
+        addon = oldint * rfactor  # addon is zero if oldint is zero
+        raw = oldint + addon.round(decimals=0)
         raw = raw.astype(dtype=np.int64)
-        if old.min() < 0:
+        if oldint.min() < 0:
             new = raw
         else:
             new = raw.clip(lower=0)
@@ -183,10 +92,31 @@ def randomize_data(xdf, taxyear, rnseed):
             info = '{} {} {} {} {}'.format(varname, old.dtype, old.min(),
                                            new.dtype, new.min())
             sys.stdout.write(info + '\n')
+        xdf[varname] = new
     if TRACE:
         info = 'number_variable_randomization_skips={}'.format(num_skips)
         sys.stdout.write(info + '\n')
-    return 0
+
+
+def constrain_data(xdf):
+    """
+    Constrains data variable values as required by Records class
+
+    Parameters
+    ----------
+    xdf: Pandas DataFrame
+        contains randomized data to be constrained.
+    """
+    if DEBUG:
+        return
+    # constraint: e00200 = e00200p + e00200s
+    xdf['e00200'] = xdf['e00200p'] + xdf['e00200s']
+    # constraint: e00900 = e00900p + e00900s
+    xdf['e00900'] = xdf['e00900p'] + xdf['e00900s']
+    # constraint: e02100 = e02100p + e02100s
+    xdf['e02100'] = xdf['e02100p'] + xdf['e02100s']
+    # constraint: e00600 >= e00650
+    xdf['e00600'] = xdf['e00600'].clip(lower=xdf['e00650'])
 
 
 def main(taxyear, rnseed, ssize):
@@ -216,9 +146,10 @@ def main(taxyear, rnseed, ssize):
         sys.stdout.write(info + '\n')
 
     # add random amounts to xdf variables
-    rtncode = randomize_data(xdf, taxyear, rnseed)
-    if rtncode != 0:
-        return rtncode
+    randomize_data(xdf, taxyear, rnseed)
+
+    # constrain values of certain variables as required by Records class
+    constrain_data(xdf)
 
     # sample xdf without replacement to get ssize observations
     if DEBUG:
