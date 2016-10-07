384615e7efc5f01874b2a312006d99219311666e
Cody Kallen <cody.kallen@aei.org>
2016-10-07 12:31:45 -0400

    corrected PEP8 errors

--- taxcalc/functions.py
+++ taxcalc/functions.py
@@ -154,9 +154,9 @@ def CapGains(p23250, p22250, _sep, ALD_Investment_ec, ALD_StudentLoan_HC,
     # limitation on capital losses
     c01000 = max((-3000. / _sep), c23650)
     # compute ymod* variables
-    ymod1 = (e00200 + e00700 + e00800 + e00900+ e01400 + e01700 +
-             (1 - ALD_Investment_ec) * 
-             (e00300 + e00600 + c01000 + e01100 + e01200) + 
+    ymod1 = (e00200 + e00700 + e00800 + e00900 + e01400 + e01700 +
+             (1 - ALD_Investment_ec) *
+             (e00300 + e00600 + c01000 + e01100 + e01200) +
              e02000 + e02100 + e02300)
     ymod2 = e00400 + (0.50 * e02400) - c02900
     ymod3 = (1 - ALD_StudentLoan_HC) * e03210 + e03230 + e03240
