2c60d40ec9c7f1b1d940ecbb632ba09da1729805
martinholmer <martin.holmer@gmail.com>
2017-03-08 08:24:37 -0500

    Absolute import in dropq/__init__.py

--- taxcalc/dropq/__init__.py
+++ taxcalc/dropq/__init__.py
@@ -1,5 +1,8 @@
-from dropq import (run_model, run_nth_year_model, check_user_mods,
-                   run_gdp_elast_model, run_nth_year_gdp_elast_model,
-                   calculate_baseline_and_reform,
-                   chooser, drop_records,
-                   format_macro_results, groupby_means_and_comparisons)
+from taxcalc.dropq.dropq import (run_model, check_user_mods,
+                                 run_nth_year_model,
+                                 run_gdp_elast_model,
+                                 run_nth_year_gdp_elast_model,
+                                 calculate_baseline_and_reform,
+                                 chooser, drop_records,
+                                 format_macro_results,
+                                 groupby_means_and_comparisons)
