13f5b2b8c2a053299913b646a367467c4463a628
martinholmer <martin.holmer@gmail.com>
2017-03-09 08:10:49 -0500

    Revise tc.py file again

--- tc.py
+++ tc.py
@@ -19,28 +19,17 @@ def main():
         prog='python tc.py',
         description=('Writes to a file the federal income and payroll tax '
                      'OUTPUT for each filing unit specified in the INPUT '
-                     'file, with the OUTPUT computed from the INPUT for '
-                     'the TAXYEAR using Tax-Calculator. '
-                     'The INPUT file is a CSV-formatted file that contains '
-                     'for each filing unit variables that are needed to '
-                     'compute tax liability.  The OUTPUT file is a '
-                     'CSV-formatted file that contains calculated tax '
-                     'information for each INPUT filing unit.  The name of '
-                     'the OUTPUT file is the INPUT file name (excluding '
-                     'directory path prefix and .csv suffix), followed by '
-                     '-YY (where YY is the last two digits in the TAXYEAR) '
-                     'followed by -REFORM (excluding any .json ending to '
-                     'the REFORM file name) or nothing if no --reform is '
-                     'specified, followed by -ASSUMP (excluding any .json '
-                     'ending to the ASSUMP file name) or nothing if no '
-                     '--assump option is specified.'))
+                     'file, with the OUTPUT computed from the INPUT for the '
+                     'TAXYEAR using Tax-Calculator. The OUTPUT file is a '
+                     'CSV-formatted file that contains tax information for '
+                     'each INPUT filing unit.'))
     parser.add_argument('INPUT', nargs='?', default='',
-                        help=('INPUT is name of required CSV file that '
+                        help=('INPUT is name of CSV-formatted file that '
                               'contains for each filing unit variables used '
                               'to compute taxes for TAXYEAR.'))
     parser.add_argument('TAXYEAR', nargs='?', default=0,
                         help=('TAXYEAR is calendar year for which taxes '
-                              'are computed (e.g., 2018).'),
+                              'are computed.'),
                         type=int)
     parser.add_argument('--reform',
                         help=('REFORM is name of optional JSON reform file. '
@@ -52,12 +41,6 @@ def main():
                               'assumption file.  No --assump implies use of '
                               'static analysis assumptions.'),
                         default=None)
-    parser.add_argument('--output',
-                        help=('OUTPUT specifies which kind of output '
-                              'variables are included in the CSV-formatted '
-                              'OUTPUT file.  No --output implies minimal tax '
-                              'output.'),
-                        default=None)
     parser.add_argument('--exact',
                         help=('optional flag to suppress smoothing in income '
                               'tax calculations that eliminate marginal-tax-'
@@ -66,15 +49,9 @@ def main():
                               'exact calculations called for on tax forms.'),
                         default=False,
                         action="store_true")
-    parser.add_argument('--fullcomp',
-                        help=('optional flag that causes OUTPUT to have '
-                              'marginal tax rates (MTRs) calculated with '
-                              'respect to full compensation (but any '
-                              'behavioral-response calculations always use '
-                              'MTRs that are calculated with respect to full '
-                              'compensation).  No --fullcomp flag implies '
-                              'MTRs reported in OUTPUT are not calculated '
-                              'with respect to full compensation.'),
+    parser.add_argument('--graph',
+                        help=('optional flag that causes HTML graphs to be '
+                              'generated.'),
                         default=False,
                         action="store_true")
     parser.add_argument('--ceeu',
@@ -82,9 +59,15 @@ def main():
                               'statistics, including certainty-equivalent '
                               'expected-utility of after-tax income values '
                               'for different constant-relative-risk-aversion '
-                              'parameter values, to be written to stdout.  '
-                              'No --ceeu flag implies nothing is written '
-                              'to stdout.'),
+                              'parameter values, to be written to screen.'),
+                        default=False,
+                        action="store_true")
+    parser.add_argument('--dump',
+                        help=('optional flag that causes OUTPUT to contain '
+                              'all INPUT variables (possibly aged to TAXYEAR) '
+                              'and all calculated tax variables, where the '
+                              'variables are named using their internal '
+                              'Tax-Calculator names.'),
                         default=False,
                         action="store_true")
     args = parser.parse_args()
@@ -98,28 +81,38 @@ def main():
         sys.stderr.write('ERROR: must specify TAXYEAR >= 2013;\n')
         sys.stderr.write('USAGE: python tc.py --help\n')
         return 1
-    # check consistency of REFORM and ASSUMP options
+    # check consistency of --reform and --assump options
     if args.assump and not args.reform:
-        sys.stderr.write('ERROR: cannot specify ASSUMP without REFORM\n')
+        sys.stderr.write('ERROR: cannot use --assump without --reform\n')
+        sys.stderr.write('USAGE: python tc.py --help\n')
+        return 1
+    # check consistency of --reform and --graph options
+    if args.graph and not args.reform:
+        sys.stderr.write('ERROR: cannot specify --graph without --reform\n')
+        sys.stderr.write('USAGE: python tc.py --help\n')
+        return 1
+    # check consistency of --reform and --ceeu options
+    if args.ceeu and not args.reform:
+        sys.stderr.write('ERROR: cannot specify --ceeu without --reform\n')
         sys.stderr.write('USAGE: python tc.py --help\n')
         return 1
     # instantiate TaxCalcIO object and do federal tax calculations
     if args.INPUT == 'puf.csv' or args.INPUT == 'cps.csv':
-        aging_and_weights = True
+        aging_input = True
     else:
-        aging_and_weights = False
+        aging_input = False
     tcio = TaxCalcIO(input_data=args.INPUT,
                      tax_year=args.TAXYEAR,
                      reform=args.reform,
                      assump=args.assump,
-                     aging_input_data=aging_and_weights,
+                     aging_input_data=aging_input,
                      exact_calculations=args.exact,
                      output_records=False,
                      csv_dump=False)
     tcio.calculate(writing_output_file=True,
                    exact_output=args.exact,
-                   output_weights=aging_and_weights,
-                   output_mtr_wrt_fullcomp=args.fullcomp,
+                   output_weights=True,  # TODO: remove this argument
+                   output_mtr_wrt_fullcomp=False,  # TODO: remove this argument
                    output_ceeu=args.ceeu)
     # return no-error exit code
     return 0
