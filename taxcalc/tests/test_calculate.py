c6cadd09430d752588245aaa06c41619e4bb1beb
Cody Kallen <cody.kallen@aei.org>
2017-05-12 17:41:38 -0400

    Weighted average MTR function

--- taxcalc/tests/test_calculate.py
+++ taxcalc/tests/test_calculate.py
@@ -210,6 +210,21 @@ def test_Calculator_mtr(records_2009):
     assert type(mtr_combined) == np.ndarray
 
 
+def test_Calculator_wmtr(records_2009):
+    calc = Calculator(policy=Policy(), records=records_2009)
+    recs_pre_e00200p = copy.deepcopy(calc.records.e00200p)
+    # test calling Calculator.wmtr on several income types
+    assert type(wmtr(income_type='e00200p')) == numpy.float64
+    assert type(wmtr(income_type='p23250')) == numpy.float64
+    assert type(wmtr(income_type='e00650')) == numpy.float64
+    # Since e00200p is non-negative, test that different methods
+    #    to control for that produce the same result
+    wmtr1 = wmtr(income_type='e00200p', weight_type='income')
+    wmtr2 = wmtr(income_type='e00200p', weight_type='abs')
+    wmtr3 = wmtr(income_type='e00200p', pos_control='posinc')
+    assert np.allclose(wmtr1, wmtr2, wmtr3)
+
+
 def test_Calculator_mtr_when_PT_rates_differ():
     reform = {2013: {'_II_rt1': [0.40],
                      '_II_rt2': [0.40],
