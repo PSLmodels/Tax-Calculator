5d7acf34fc61831d2f6506faff2b2667e8a5c2ed
martinholmer <martin.holmer@gmail.com>
2017-06-15 12:43:14 -0400

    New test not executed on GitHub

--- taxcalc/tests/test_4package.py
+++ taxcalc/tests/test_4package.py
@@ -3,9 +3,12 @@ import subprocess
 import pytest
 
 
+@pytest.mark.requires_pufcsv
 def test_for_package_existence():
     """
-    Ensure that no conda taxcalc package is installed when running pytest
+    Ensure that no conda taxcalc package is installed when running pytest.
+    Primarily to help developers catch mistaken installations of taxcalc;
+    the requires_pufcsv mark prevents test from running on GitHub.
     """
     out = subprocess.check_output(['conda', 'list', 'taxcalc']).decode('ascii')
     envless_out = out.replace('taxcalc-dev', 'environment')
