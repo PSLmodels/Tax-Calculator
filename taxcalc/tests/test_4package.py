7b9dbee7a0b33f98af5060611e2c07ac805e1cc0
martinholmer <martin.holmer@gmail.com>
2017-06-15 10:47:20 -0400

    Filter out taxcalc-dev environment in package search

--- taxcalc/tests/test_4package.py
+++ taxcalc/tests/test_4package.py
@@ -7,6 +7,7 @@ def test_for_package_existence():
     """
     Ensure that no conda taxcalc package is installed when running pytest
     """
-    out = subprocess.check_output(['conda', 'list', 'taxcalc'])
-    if re.search('taxcalc', out.decode('ascii')) is not None:
+    out = subprocess.check_output(['conda', 'list', 'taxcalc']).decode('ascii')
+    envless_out = out.replace('taxcalc-dev', 'environment')
+    if re.search('taxcalc', envless_out) is not None:
         assert 'taxcalc package' == 'installed'
