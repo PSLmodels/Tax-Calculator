e395b529d5cfcf76cc0a4db3d30a655fe82902b5
martinholmer <martin.holmer@gmail.com>
2017-06-15 08:51:20 -0400

    Revise yet again new unit test logic

--- taxcalc/tests/test_4package.py
+++ taxcalc/tests/test_4package.py
@@ -2,11 +2,11 @@ import re
 import subprocess
 import pytest
 
-
+@pytest.mark.one
 def test_for_package_existence():
     """
     Ensure that no conda taxcalc package is installed when running pytest
     """
     out = subprocess.check_output(['conda', 'list', 'taxcalc'])
-    if re.search('taxcalc'.encode('ascii'), out.encode('ascii')) is not None:
+    if re.search('taxcalc', out.decode('ascii')) is not None:
         assert 'taxcalc package' == 'installed'
