7ce735734550c83286fc0284ebb11fc14a9591f8
martinholmer <martin.holmer@gmail.com>
2017-08-30 13:14:45 -0400

    Remove obsolete tests from test_dropq.py

--- taxcalc/tests/test_dropq.py
+++ taxcalc/tests/test_dropq.py
@@ -2,14 +2,12 @@
 test_dropq.py uses only PUF input data because the dropq algorithm
 is designed to work exclusively with private IRS-SOI PUF input data.
 """
-import six
 import numpy as np
 import pandas as pd
 import pytest
 from taxcalc.dropq.dropq_utils import *
 from taxcalc.dropq import *
 from taxcalc import (Policy, Records, Calculator,
-                     create_difference_table,
                      multiyear_diagnostic_table, results)
 
 
@@ -166,37 +164,6 @@ def test_dropq_dist_table(groupby, result_type, puf_subsample):
 
 
 @pytest.mark.requires_pufcsv
-@pytest.mark.parametrize('groupby, tax_to_diff',
-                         [('weighted_deciles', 'iitax'),
-                          ('webapp_income_bins', 'payrolltax'),
-                          ('other_deciles', 'combined')])
-def test_dropq_diff_table(groupby, tax_to_diff, puf_subsample):
-    recs1 = Records(data=puf_subsample)
-    calc1 = Calculator(policy=Policy(), records=recs1)
-    recs2 = Records(data=puf_subsample)
-    pol2 = Policy()
-    pol2.implement_reform(USER_MODS['policy'])
-    calc2 = Calculator(policy=pol2, records=recs2)
-    calc1.calc_all()
-    calc2.calc_all()
-    res1 = results(calc1.records)
-    res2 = results(calc2.records)
-    assert len(res1.index) == len(res2.index)
-    mask = np.ones(len(res1.index))
-    (res1, res2) = drop_records(res1, res2, mask)
-    if groupby == 'other_deciles':
-        with pytest.raises(ValueError):
-            create_difference_table(res1, res2, groupby=groupby,
-                                    income_measure='expanded_income',
-                                    tax_to_diff=tax_to_diff)
-    else:
-        diffs = create_difference_table(res1, res2, groupby=groupby,
-                                        income_measure='expanded_income',
-                                        tax_to_diff=tax_to_diff)
-        assert isinstance(diffs, pd.DataFrame)
-
-
-@pytest.mark.requires_pufcsv
 def test_with_pufcsv(puf_fullsample):
     # specify usermods dictionary in code
     start_year = 2017
@@ -272,59 +239,3 @@ def test_reform_warnings_errors():
     msg_dict = reform_warnings_errors(bad2_mods)
     assert len(msg_dict['warnings']) == 0
     assert len(msg_dict['errors']) > 0
-
-
-@pytest.mark.requires_pufcsv
-def test_dropq_diff_vs_util_diff(puf_subsample):
-    recs1 = Records(data=puf_subsample)
-    calc1 = Calculator(policy=Policy(), records=recs1)
-    recs2 = Records(data=puf_subsample)
-    pol2 = Policy()
-    pol2.implement_reform(USER_MODS['policy'])
-    calc2 = Calculator(policy=pol2, records=recs2)
-    calc1.advance_to_year(2016)
-    calc2.advance_to_year(2016)
-    calc1.calc_all()
-    calc2.calc_all()
-    # generate diff table using utility function
-    udf = create_difference_table(calc1.records, calc2.records,
-                                  groupby='weighted_deciles',
-                                  income_measure='expanded_income',
-                                  tax_to_diff='iitax')
-    assert isinstance(udf, pd.DataFrame)
-    # generate diff table using dropq approach without dropping any records
-    res1 = results(calc1.records)
-    res2 = results(calc2.records)
-    qdf = create_difference_table(res1, res2,
-                                  groupby='weighted_deciles',
-                                  income_measure='expanded_income',
-                                  tax_to_diff='iitax')
-    assert isinstance(qdf, pd.DataFrame)
-    # check that each element in the two DataFrames are the same
-    assert udf.shape[0] == qdf.shape[0]  # same number of rows
-    assert udf.shape[1] == qdf.shape[1]  # same number of cols
-    for col in list(qdf):
-        for row in range(0, qdf.shape[0]):
-            same = False
-            qel_str_type = isinstance(qdf[col][row], six.string_types)
-            uel_str_type = isinstance(udf[col][row], six.string_types)
-            assert qel_str_type == uel_str_type
-            if qel_str_type:
-                same = qdf[col][row] == udf[col][row]
-            else:
-                qel_flt_type = isinstance(qdf[col][row], float)
-                uel_flt_type = isinstance(udf[col][row], float)
-                assert qel_flt_type == uel_flt_type
-                if qel_flt_type:
-                    same = np.allclose([qdf[col][row]], [udf[col][row]])
-            if not same:
-                msg = '{} {} : [{}] {} [{}] {}'.format(col, row,
-                                                       qdf[col][row],
-                                                       type(qdf[col][row]),
-                                                       udf[col][row],
-                                                       type(udf[col][row]))
-                assert msg == 'qdf element not equal to udf element'
-    # uncomment the following three lines to force print of udf contents
-    # for col in sorted(list(udf)):
-    #     print udf[col]
-    # assert 1 == 2
