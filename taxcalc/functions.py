422e93b84ff22d20a277e322f74b1b61050dfd6f
Cody Kallen <cody.kallen@aei.org>
2017-11-02 10:49:25 -0400

    modify for backward compatibility

--- taxcalc/functions.py
+++ taxcalc/functions.py
@@ -1100,7 +1100,7 @@ def ChildTaxCredit(n24, MARS, c00100, exact,
                    CTC_c, CTC_ps, CTC_prt, prectc, nu05,
                    CTC_c_under5_bonus, XTOT, num,
                    DependentCredit_Child_c, DependentCredit_Nonchild_c,
-                   dep_credit):
+                   DependentCredit_c, dep_credit):
     """
     ChildTaxCredit function computes prectc amount and dependent credit
     """
@@ -1113,7 +1113,8 @@ def ChildTaxCredit(n24, MARS, c00100, exact,
             excess = 1000. * math.ceil(excess / 1000.)
         prectc = max(0., prectc - CTC_prt * excess)
     # calculate and phase-out dependent credit
-    dep_credit = (DependentCredit_Child_c * n24 +
+    dep_credit = (DependentCredit_c * max(0, XTOT - num) +
+                  DependentCredit_Child_c * n24 +
                   DependentCredit_Nonchild_c * max(0, XTOT - n24 - num))
     if CTC_prt > 0. and c00100 > CTC_ps[MARS - 1]:
         thresh = CTC_ps[MARS - 1] + n24 * CTC_c / CTC_prt
