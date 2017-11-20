0e62256abfe19ef2e2300e9e6c06741e55ba3555
Martin Holmer <martin.holmer@gmail.com>
2017-11-20 10:42:03 -0500

    Rename docs/make.py as docs/make_index.py

--- docs/make_index.py
+++ docs/make_index.py
@@ -3,8 +3,8 @@ Reads skeletal index.htmx file and writes fleshed-out index.html file
 containing information from several JSON files.
 """
 # CODING-STYLE CHECKS:
-# pep8 --ignore=E402 make.py
-# pylint --disable=locally-disabled make.py
+# pep8 --ignore=E402 make_index.py
+# pylint --disable=locally-disabled make_index.py
 
 import os
 import sys
