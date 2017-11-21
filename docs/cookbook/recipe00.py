6ea5277dc70584ebe1fac95dae9a837ced4f2b53
Martin Holmer <martin.holmer@gmail.com>
2017-11-21 09:44:09 -0500

    Revise cookbook/recipe00.py and add recipe00.res file

--- docs/cookbook/recipe00.py
+++ docs/cookbook/recipe00.py
@@ -1,7 +1,3 @@
-"""
-RECIPE00.PY
-"""
-
 from __future__ import print_function  # necessary only if using Python 2.7
 
 from taxcalc import *
@@ -14,30 +10,36 @@ recs = Records.cps_constructor()
 pol = Policy()
 calc1 = Calculator(policy=pol, records=recs)
 
-# read JSON reform file and use default (that is, static) analysis assumptions
+# NOTE: calc1 now contains a PRIVATE COPY of pol and a PRIVATE COPY of recs,
+#       so we can continue to use pol and recs in this script without any
+#       concern about side effects from Calculator method calls.
+
+# calculate aggregate current-law income tax liabilities for 2018
+calc1.advance_to_year(2018)
+calc1.calc_all()
+itax_rev1 = calc1.weighted_total('iitax')
+
+# read JSON reform file and use (the default) static analysis assumptions
 reform_filename = './ingredients/repeal_amt.json'
 params = Calculator.read_json_param_objects(reform=reform_filename,
                                             assump=None)
 
 # specify Calculator object for static analysis of reform policy
 pol.implement_reform(params['policy'])
-if pol.reform_errors != '':  # check for reform error messages
+if pol.reform_errors:  # check for reform error messages
     print(pol.reform_errors)
     exit(1)
 calc2 = Calculator(policy=pol, records=recs)
 
-# calculate tax liabilities for 2018
-calc1.advance_to_year(2018)
-calc1.calc_all()
+# calculate reform income tax liabilities for 2018
 calc2.advance_to_year(2018)
 calc2.calc_all()
+itax_rev2 = calc2.weighted_total('iitax')
 
-# compute total income tax liability under current law and the reform
-# (records.iitax contains each filing unit's individual income tax liability
-# and records.s006 contains each filing units's sampling weight)
-itax_rev1 = (calc1.records.iitax * calc1.records.s006).sum()
-itax_rev2 = (calc2.records.iitax * calc2.records.s006).sum()
+# print reform documentation
+print(Calculator.reform_documentation(params))
 
-# print total revenue estimates for 2018 in whole billons of dollars
-print('2018_CLP_itax_rev($B)= {:.0f}'.format(itax_rev1 * 1e-9))
-print('2018_REF_itax_rev($B)= {:.0f}'.format(itax_rev2 * 1e-9))
+# print total revenue estimates for 2018
+# (estimates in billons of dollars rounded to nearest tenth of a billion)
+print('2018_CLP_itax_rev($B)= {:.1f}'.format(itax_rev1 * 1e-9))
+print('2018_REF_itax_rev($B)= {:.1f}'.format(itax_rev2 * 1e-9))
---
+++ docs/cookbook/recipe00.res
@@ -0,0 +1,55 @@
+You loaded data for 2014.
+Tax-Calculator startup automatically extrapolated your data to 2014.
+You loaded data for 2014.
+Tax-Calculator startup automatically extrapolated your data to 2014.
+REFORM DOCUMENTATION
+Baseline Growth-Difference Assumption Values by Year:
+none: using default baseline growth assumptions
+Policy Reform Parameter Values by Year:
+2018:
+ _AMT_rt1 : 0.0
+  name: AMT rate 1
+  desc: The tax rate applied to the portion of AMT taxable income below the
+        surtax threshold, AMT bracket 1.
+  baseline_value: 0.26
+ _AMT_rt2 : 0.0
+  name: Additional AMT rate for AMT taxable income above AMT bracket 1
+  desc: The additional tax rate applied to the portion of AMT income above the
+        AMT bracket 1.
+  baseline_value: 0.02
+ _II_rt5 : 0.35
+  name: Personal income (regular/non-AMT/non-pass-through) tax rate 5
+  desc: The third highest tax rate, applied to the portion of taxable income
+        below tax bracket 5 and above tax bracket 4.
+  baseline_value: 0.33
+ _II_rt6 : 0.37
+  name: Personal income (regular/non-AMT/non-pass-through) tax rate 6
+  desc: The second higher tax rate, applied to the portion of taxable income
+        below tax bracket 6 and above tax bracket 5.
+  baseline_value: 0.35
+ _II_rt7 : 0.42
+  name: Personal income (regular/non-AMT/non-pass-through) tax rate 7
+  desc: The tax rate applied to the portion of taxable income below tax
+        bracket 7 and above tax bracket 6.
+  baseline_value: 0.396
+ _PT_rt5 : 0.35
+  name: Pass-through income tax rate 5
+  desc: The third highest tax rate, applied to the portion of income from sole
+        proprietorships, partnerships and S corporations below tax bracket 5
+        and above tax bracket 4.
+  baseline_value: 0.33
+ _PT_rt6 : 0.37
+  name: Pass-through income tax rate 6
+  desc: The second higher tax rate, applied to the portion of income from sole
+        proprietorships, partnerships and S corporations below tax bracket 6
+        and above tax bracket 5.
+  baseline_value: 0.35
+ _PT_rt7 : 0.42
+  name: Pass-through income tax rate 7
+  desc: The highest tax rate, applied to the portion of income from sole
+        proprietorships, partnerships and S corporations below tax bracket 7
+        and above tax bracket 6.
+  baseline_value: 0.396
+
+2018_CLP_itax_rev($B)= 1287.5
+2018_REF_itax_rev($B)= 1283.1
