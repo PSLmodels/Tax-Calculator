3b5db46b464f5669a47e70a172b6ecd3d0577e7e
Martin Holmer <martin.holmer@gmail.com>
2017-11-21 15:35:20 -0500

    Fix correlation coefficient matrix column ordering

--- taxcalc/tests/test_puf_var_stats.py
+++ taxcalc/tests/test_puf_var_stats.py
@@ -149,6 +149,7 @@ def test_puf_var_stats(tests_path, puf_fullsample):
     # create base tables
     table_mean = create_base_table(tests_path)
     table_corr = copy.deepcopy(table_mean)
+    del table_corr['description']
     # add statistics to tables
     year_headers = ['description']
     for year in range(Policy.JSON_START_YEAR, Policy.LAST_BUDGET_YEAR + 1):
@@ -166,7 +167,8 @@ def test_puf_var_stats(tests_path, puf_fullsample):
     table_mean.to_csv(mean_path, header=year_headers, float_format='%8.3f')
     corr_path = os.path.join(tests_path, CORR_FILENAME + '-new')
     table_corr.sort_index(inplace=True)
-    table_corr.to_csv(corr_path, float_format='%8.3f')
+    table_corr.to_csv(corr_path, float_format='%8.3f',
+                      columns=table_corr.index)
     # compare new and old CSV files
     diffs_in_mean, mean_msg = differences(mean_path, mean_path[:-4], 'MEAN')
     diffs_in_corr, corr_msg = differences(corr_path, corr_path[:-4], 'CORR')
