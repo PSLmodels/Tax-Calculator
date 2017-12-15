7da2750fef9ec4f97d3da808970c8400563a7d5f
Cody Kallen <cody.kallen@aei.org>
2017-12-15 09:14:41 -0500

    Add cap on state and local income, sales and real estate tax deduction

--- taxcalc/functions.py
+++ taxcalc/functions.py
@@ -465,7 +465,7 @@ def ItemDed(e17500_capped, e18400_capped, e18500_capped,
             ID_Charity_hc, ID_InterestPaid_hc, ID_RealEstate_hc,
             ID_Medical_c, ID_StateLocalTax_c, ID_RealEstate_c,
             ID_InterestPaid_c, ID_Charity_c, ID_Casualty_c,
-            ID_Miscellaneous_c):
+            ID_Miscellaneous_c, ID_AllTaxes_c):
     """
     ItemDed function: itemized deductions, Form 1040, Schedule A
 
@@ -507,6 +507,9 @@ def ItemDed(e17500_capped, e18400_capped, e18500_capped,
         ID_StateLocalTax_c : Ceiling on state and local tax deduction
 
         ID_RealEstate_c : Ceiling on real estate tax deduction
+
+        ID_AllTaxes_c: Ceiling combined state and local income/sales and
+        real estate tax deductions
 
         ID_InterestPaid_c : Ceiling on interest paid deduction
 
@@ -551,7 +554,7 @@ def ItemDed(e17500_capped, e18400_capped, e18500_capped,
                  ID_StateLocalTax_c[MARS - 1])
     c18500 = min((1. - ID_RealEstate_hc) * e18500_capped,
                  ID_RealEstate_c[MARS - 1])
-    c18300 = c18400 + c18500
+    c18300 = min(c18400 + c18500, ID_AllTaxes_c[MARS - 1])
     # Interest paid
     c19200 = e19200_capped * (1. - ID_InterestPaid_hc)
     c19200 = min(c19200, ID_InterestPaid_c[MARS - 1])
