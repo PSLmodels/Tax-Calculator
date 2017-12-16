74dfa4e5ea7a38e47897f0ff73f799b8b00d259d
martinholmer <martin.holmer@gmail.com>
2017-12-16 15:20:12 -0500

    Expand scope of testing of reforms in taxcalc/reforms

--- taxcalc/tests/test_reforms.py
+++ taxcalc/tests/test_reforms.py
@@ -5,6 +5,7 @@ Test example JSON policy reform files in taxcalc/reforms directory
 # pep8 --ignore=E402 test_reforms.py
 # pylint --disable=locally-disabled test_reforms.py
 
+from __future__ import print_function
 import os
 import glob
 import json
@@ -13,12 +14,41 @@ from taxcalc import Calculator, Policy  # pylint: disable=import-error
 from taxcalc import Records, Behavior  # pylint: disable=import-error
 
 @pytest.mark.one
-def test_reform_json(tests_path):
+def test_reform_json_and_output(tests_path):
     """
     Check that each JSON reform file can be converted into a reform dictionary
     that can then be passed to the Policy class implement_reform() method that
     generates no reform_errors.
+    Then use each reform to generate static tax results for small set of
+    filing units in a single tax_year and compare those results with
+    expected results from a text file.
     """
+    # pylint: disable=too-many-locals
+    # set tracing to True to see test output rather than writing to files
+    tracing = False # setting to True force test failure
+    # specify Records object containing cases data
+    tax_year = 2020
+    cases_path = os.path.join(tests_path, '..', 'reforms', 'cases.csv')
+    cases = Records(data=cases_path,
+                    gfactors=None,  # keeps raw data unchanged
+                    weights=None,
+                    adjust_ratios=None,
+                    start_year=tax_year)  # set raw input data year
+    # specify current-law-policy Calculator object
+    calc1 = Calculator(policy=Policy(), records=cases, verbose=False)
+    calc1.advance_to_year(tax_year)
+    calc1.calc_all()
+    dist1, _ = calc1.distribution_tables(calc=None,
+                                         groupby='large_income_bins')
+    if tracing:
+        print('TRACING: current-law-policy distribution table:')
+        print(dist1)
+    else:
+        outfilename = 'clp.out'
+        out_path = os.path.join(tests_path, '..', 'reforms', 'clp.out')
+        with open(out_path,'w') as outfile:
+            dist1.to_string(outfile)
+    # check reform file contents and reform results for each reform
     reforms_path = os.path.join(tests_path, '..', 'reforms', '*.json')
     for jrf in glob.glob(reforms_path):
         # read contents of jrf (JSON reform file)
@@ -33,12 +63,30 @@ def test_reform_json(tests_path):
                 Calculator._read_json_policy_reform_text(jrf_text,
                                                          gdiffbase, gdiffresp)
             )
-            policy = Policy()
-            policy.implement_reform(policy_dict)
+            pol = Policy()
+            pol.implement_reform(policy_dict)
+            calc2 = Calculator(policy=pol, records=cases, verbose=False)
+            calc2.advance_to_year(tax_year)
+            calc2.calc_all()
+            diff = calc1.difference_table(calc2,
+                                          groupby='large_income_bins')
+            del diff['perc_aftertax']
+            if tracing:
+                print('TRACING: difference table '
+                      'for reform in {}:'.format(os.path.basename(jrf)))
+                print(diff)
+            else:
+                outname = os.path.basename(jrf).replace('.json', '.out')
+                out_path = os.path.join(tests_path, '..', 'reforms', outname)
+                with open(out_path,'w') as outfile:
+                    diff.to_string(outfile)
         else:  # jrf_text has no "policy" key
             print('ERROR: missing policy key in file: ' +
                   os.path.basename(jrf))
             assert False
+    if tracing:
+        print('TRACING: end-of-test failure so can see printed output')
+        assert 1 == 2
 
 
 def reform_results(reform_dict, puf_data):
