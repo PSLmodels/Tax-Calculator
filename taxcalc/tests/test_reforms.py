2aeadfdc3aa02c07bedaca2a9f130519296ef4c4
martinholmer <martin.holmer@gmail.com>
2017-12-16 12:01:36 -0500

    Streamline test_reform_json logic

--- taxcalc/tests/test_reforms.py
+++ taxcalc/tests/test_reforms.py
@@ -12,31 +12,32 @@ import pytest
 from taxcalc import Calculator, Policy  # pylint: disable=import-error
 from taxcalc import Records, Behavior  # pylint: disable=import-error
 
-
+@pytest.mark.one
 def test_reform_json(tests_path):
     """
     Check that each JSON reform file can be converted into a reform dictionary
-    that can then be passed to the Policy class implement_reform() method.
+    that can then be passed to the Policy class implement_reform() method that
+    generates no reform_errors.
     """
     reforms_path = os.path.join(tests_path, '..', 'reforms', '*.json')
-    for jpf in glob.glob(reforms_path):
-        # read contents of jpf (JSON parameter filename)
-        jfile = open(jpf, 'r')
-        jpf_text = jfile.read()
-        # check that jpf_text has "policy" that can be implemented as a reform
-        if '"policy"' in jpf_text:
+    for jrf in glob.glob(reforms_path):
+        # read contents of jrf (JSON reform file)
+        with open(jrf, 'r') as jfile:
+            jrf_text = jfile.read()
+        # check that jrf_text has "policy" that can be implemented as a reform
+        if '"policy"' in jrf_text:
             gdiffbase = {}
             gdiffresp = {}
             # pylint: disable=protected-access
             policy_dict = (
-                Calculator._read_json_policy_reform_text(jpf_text,
+                Calculator._read_json_policy_reform_text(jrf_text,
                                                          gdiffbase, gdiffresp)
             )
             policy = Policy()
             policy.implement_reform(policy_dict)
-        else:  # jpf_text is not a valid JSON policy reform file
-            print('test-failing-filename: ' +
-                  jpf)
+        else:  # jrf_text has no "policy" key
+            print('ERROR: missing policy key in file: ' +
+                  os.path.basename(jrf))
             assert False
 
 
