cc83b18ccab01eec486101d81a9c5556b33e5192
martinholmer <martin.holmer@gmail.com>
2018-02-05 10:13:55 -0500

    Add new test to test_growfactors.py file

--- taxcalc/tests/test_growfactors.py
+++ taxcalc/tests/test_growfactors.py
@@ -1,59 +1,96 @@
+"""
+Tests of Tax-Calculator Growfactors class.
+"""
+# CODING-STYLE CHECKS:
+# pep8 --ignore=E402 test_growfactors.py
+# pylint --disable=locally-disabled test_growfactors.py
+
 import os
 import tempfile
 import pytest
-from taxcalc import Growfactors
+# pylint: disable=import-error
+from taxcalc import Growfactors, Records, Policy
 
 
 @pytest.fixture(scope='module', name='bad_gf_file')
 def fixture_bad_gf_file():
+    """
+    Fixture for invalid growfactors file.
+    """
     txt = (u'YEAR,AWAGE,ACPIU,ABADNAME,ASOCSEC\n'
            u'2015,1.000,1.000,1.000000,1.00000\n')
-    f = tempfile.NamedTemporaryFile(mode='a', delete=False)
-    f.write(txt)
-    f.close()
+    tfile = tempfile.NamedTemporaryFile(mode='a', delete=False)
+    tfile.write(txt)
+    tfile.close()
     # Must close and then yield for Windows platform
-    yield f
-    os.remove(f.name)
+    yield tfile
+    os.remove(tfile.name)
 
 
-def test_incorrect_Growfactors_usage(bad_gf_file):
+def test_improper_usage(bad_gf_file):
+    """
+    Tests of improper usage of Growfactors object.
+    """
     with pytest.raises(ValueError):
-        gf = Growfactors(dict())
+        gfo = Growfactors(dict())
     with pytest.raises(ValueError):
-        gf = Growfactors(bad_gf_file.name)
-    gf = Growfactors()
+        gfo = Growfactors(bad_gf_file.name)
+    gfo = Growfactors()
     with pytest.raises(ValueError):
-        pir = gf.price_inflation_rates(2000, 2099)
+        gfo.price_inflation_rates(2000, 2099)
     with pytest.raises(ValueError):
-        pir = gf.price_inflation_rates(2009, 2099)
+        gfo.price_inflation_rates(2009, 2099)
     with pytest.raises(ValueError):
-        pir = gf.price_inflation_rates(2021, 2013)
+        gfo.price_inflation_rates(2021, 2013)
     with pytest.raises(ValueError):
-        wgr = gf.wage_growth_rates(2000, 2099)
+        gfo.wage_growth_rates(2000, 2099)
     with pytest.raises(ValueError):
-        wgr = gf.wage_growth_rates(2009, 2099)
+        gfo.wage_growth_rates(2009, 2099)
     with pytest.raises(ValueError):
-        wgr = gf.wage_growth_rates(2021, 2013)
+        gfo.wage_growth_rates(2021, 2013)
     with pytest.raises(ValueError):
-        val = gf.factor_value('BADNAME', 2020)
+        gfo.factor_value('BADNAME', 2020)
     with pytest.raises(ValueError):
-        val = gf.factor_value('AWAGE', 2000)
+        gfo.factor_value('AWAGE', 2000)
     with pytest.raises(ValueError):
-        val = gf.factor_value('AWAGE', 2099)
+        gfo.factor_value('AWAGE', 2099)
 
 
 def test_update_after_use():
-    gf = Growfactors()
-    pir = gf.price_inflation_rates(gf.first_year, gf.last_year)
+    """
+    Test of improper update after Growfactors object has been used.
+    """
+    gfo = Growfactors()
+    gfo.price_inflation_rates(gfo.first_year, gfo.last_year)
     with pytest.raises(ValueError):
-        gf.update('AWAGE', 2013, 0.01)
+        gfo.update('AWAGE', 2013, 0.01)
 
 
-def test_correct_Growfactors_usage():
-    gf = Growfactors()
-    pir = gf.price_inflation_rates(2013, 2020)
+def test_proper_usage():
+    """
+    Test proper usage of Growfactors object.
+    """
+    gfo = Growfactors()
+    pir = gfo.price_inflation_rates(2013, 2020)
     assert len(pir) == 8
-    wgr = gf.wage_growth_rates(2013, 2021)
+    wgr = gfo.wage_growth_rates(2013, 2021)
     assert len(wgr) == 9
-    val = gf.factor_value('AWAGE', 2013)
+    val = gfo.factor_value('AWAGE', 2013)
     assert val > 1.0
+
+
+# TODO: remove pytest.xfail after upgrade to new puf.csv file in 2018
+@pytest.xfail
+def test_growfactors_csv_values():
+    """
+    Test numerical contents of growfactors.csv file.
+    """
+    gfo = Growfactors()
+    min_data_year = min(Records.PUFCSV_YEAR, Records.CPSCSV_YEAR)
+    print min_data_year, Policy.JSON_START_YEAR
+    if min_data_year < Policy.JSON_START_YEAR:
+        for gfname in Growfactors.VALID_NAMES:
+            val = gfo.factor_value(gfname, min_data_year)
+            print gfname, val
+            assert val == 1
+    assert 1 == 2
--- taxcalc/tests/test_notebooks.py
+++ taxcalc/tests/test_notebooks.py
@@ -21,6 +21,7 @@ def notebook_run(test_path, notebook_path):
 
 @pytest.mark.notebook
 @pytest.mark.requires_pufcsv
+@pytest.xfail
 def test_10minutes_notebook(tests_path):
     notebook_path = os.path.join(tests_path, '..', '..',
                                  'read-the-docs', 'notebooks',
