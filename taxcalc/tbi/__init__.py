8eda864b4c4f38a9752d3ed3b6ce6a5f6d252ad8
martinholmer <martin.holmer@gmail.com>
2018-03-10 12:42:05 -0500

    tbi (TaxBrain Interface) now returns full tables

--- taxcalc/tbi/__init__.py
+++ taxcalc/tbi/__init__.py
@@ -1,4 +1,3 @@
 from taxcalc.tbi.tbi import (run_nth_year_tax_calc_model,
-                             WEBBIN_ROW_NAMES,
                              run_nth_year_gdp_elast_model,
                              reform_warnings_errors)
