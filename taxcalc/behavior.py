9219114950082fa4f88a400f8047582b48003259
Henry Doupe <hdoupe@yahoo.com>
2018-04-10 10:33:59 -0400

    Revise Policy.implement_reform docstring to apply to Behavior.update_behavior

--- taxcalc/behavior.py
+++ taxcalc/behavior.py
@@ -67,17 +67,12 @@ class Behavior(ParametersBase):
 
     def update_behavior(self, reform, raise_errors=True):
         """
-        Implement multi-year policy reform and leave current_year unchanged.
+        Implement multi-year behavior reform and leave current_year unchanged.
 
         Parameters
         ----------
         reform: dictionary of one or more YEAR:MODS pairs
             see Notes to Parameters _update method for info on MODS structure
-
-        print_warnings: boolean
-            if True (the default), prints warnings when parameter_warnings exists;
-            if False, does not print warnings when parameter_warnings exists and
-                      leaves warning handling to caller of implement_reform.
 
         raise_errors: boolean
             if True (the default), raises ValueError when parameter_errors exists;
@@ -92,8 +87,11 @@ class Behavior(ParametersBase):
             if minimum YEAR in the YEAR:MODS pairs is less than start_year.
             if minimum YEAR in the YEAR:MODS pairs is less than current_year.
             if maximum YEAR in the YEAR:MODS pairs is greater than end_year.
-            if Policy._validate_parameter_names_types generates error messages.
-            if Policy._validate_parameter_values generates error messages.
+            If raise_errors is True AND:
+                if Behavior._validate_parameter_names_types generates error
+                messages.
+                if Behavior._validate_parameter_values generates error
+                messages.
 
         Returns
         -------
@@ -104,31 +102,24 @@ class Behavior(ParametersBase):
         Given a reform dictionary, typical usage of the Policy class
         is as follows::
 
-            policy = Policy()
-            policy.implement_reform(reform)
+            behavior = Behavior()
+            behavior.update_behavior(reform)
 
-        In the above statements, the Policy() call instantiates a
-        policy object (policy) containing current-law policy parameters,
-        and the implement_reform(reform) call applies the (possibly
+        In the above statements, the Behavior() call instantiates a
+        behavior object (behavior) containing behavior baseline parameters,
+        and the update_behavior(reform) call applies the (possibly
         multi-year) reform specified in reform and then sets the
-        current_year to the value of current_year when implement_reform
+        current_year to the value of current_year when update_behavior
         was called with parameters set for that pre-call year.
 
         An example of a multi-year, multi-parameter reform is as follows::
 
             reform = {
                 2016: {
-                    '_EITC_c': [[900, 5000, 8000, 9000]],
-                    '_II_em': [7000],
-                    '_SS_Earnings_c': [300000]
+                    '_BE_inc': [-0.3]
                 },
                 2017: {
-                    '_SS_Earnings_c': [500000], '_SS_Earnings_c_cpi': False
-                },
-                2019: {
-                    '_EITC_c': [[1200, 7000, 10000, 12000]],
-                    '_II_em': [9000],
-                    '_SS_Earnings_c': [700000], '_SS_Earnings_c_cpi': True
+                    '_BE_sub': [0.2]
                 }
             }
 
