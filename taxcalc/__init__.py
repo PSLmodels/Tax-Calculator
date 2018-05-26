4e1030da2d9bc418f6601eeb6c9fd7ce8eb4ebb5
martinholmer <martin.holmer@gmail.com>
2018-05-26 18:54:15 -0400

    Remove taxcalc/filings/forms code and associated tests

--- taxcalc/__init__.py
+++ taxcalc/__init__.py
@@ -5,7 +5,6 @@ from taxcalc.calculate import *
 from taxcalc.policy import *
 from taxcalc.behavior import *
 from taxcalc.consumption import *
-from taxcalc.filings import *
 from taxcalc.growfactors import *
 from taxcalc.growdiff import *
 from taxcalc.growmodel import *
