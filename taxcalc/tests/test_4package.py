26c491f7fb444cd80d6d843a4204d90313a1a66f
Martin Holmer <martin.holmer@gmail.com>
2018-09-18 15:35:18 -0400

    Add 'pip install pyyaml' to .travis.yml file

--- taxcalc/tests/test_4package.py
+++ taxcalc/tests/test_4package.py
@@ -17,7 +17,7 @@ def test_for_package_existence():
     """
     Ensure that no conda taxcalc package is installed when running pytest.
     Primarily to help developers catch mistaken installations of taxcalc;
-    the pre_release mark prevents test from running on GitHub.
+    the local mark prevents test from running on GitHub.
     """
     out = subprocess.check_output(['conda', 'list', 'taxcalc']).decode('ascii')
     envless_out = out.replace('taxcalc-dev', 'environment')
