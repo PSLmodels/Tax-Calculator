38e669a070734178e6db45b84d0f7cf710e148c6
Martin Holmer <martin.holmer@gmail.com>
2018-11-20 10:05:53 -0500

    Allow Records ctor to accept a Pandas DataFrame for adjust_ratios argument

--- taxcalc/records.py
+++ taxcalc/records.py
@@ -43,9 +43,11 @@ class Records():
 
     adjust_ratios: string or Pandas DataFrame or None
         string describes CSV file in which adjustment ratios reside;
-        DataFrame already contains adjustment ratios;
+        DataFrame already contains transposed/no-index adjustment ratios;
         None creates empty adjustment-ratios DataFrame;
         default value is filename of the PUF adjustment ratios.
+        NOTE: if using ratio_df DataFrame, read file as follows:
+        ratio_df = pandas.read_csv('csv_filename', index_col=0).transpose()
 
     start_year: integer
         specifies calendar year of the input data;
@@ -533,12 +535,17 @@ class Records():
     def _read_ratios(self, ratios):
         """
         Read Records adjustment ratios from file or
+        use specified transposed/no-index DataFrame as ratios or
         create empty DataFrame if None
         """
         if ratios is None:
             setattr(self, 'ADJ', pd.DataFrame({'nothing': []}))
             return
-        if isinstance(ratios, str):
+        if isinstance(ratios, pd.DataFrame):
+            assert 'INT2013' in ratios.columns  # check for transposed
+            assert ratios.index.name is None  # check for no-index
+            ADJ = ratios
+        elif isinstance(ratios, str):
             ratios_path = os.path.join(Records.CUR_PATH, ratios)
             if os.path.isfile(ratios_path):
                 ADJ = pd.read_csv(ratios_path,
@@ -547,11 +554,11 @@ class Records():
                 # cannot call read_egg_ function in unit tests
                 ADJ = read_egg_csv(os.path.basename(ratios_path),
                                    index_col=0)  # pragma: no cover
+            ADJ = ADJ.transpose()
         else:
-            msg = 'ratios is neither None nor a string'
+            msg = 'ratios is neither None nor a Pandas DataFrame nor a string'
             raise ValueError(msg)
         assert isinstance(ADJ, pd.DataFrame)
-        ADJ = ADJ.transpose()
         if ADJ.index.name != 'agi_bin':
             ADJ.index.name = 'agi_bin'
         self.ADJ = pd.DataFrame()
