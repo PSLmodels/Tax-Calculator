c9b05424b5f030c1e4e609fbd339f8a0084f93b9
Martin Holmer <martin.holmer@gmail.com>
2019-01-21 08:56:43 -0500

    Undo debugging changes wrt Windows GitHub pytest errors

--- continuous_integration/run_tests.cmd
+++ continuous_integration/run_tests.cmd
@@ -4,6 +4,6 @@ call activate %CONDA_ENV%
 
 set PYTHONFAULTHANDLER=1
 
-set PYTEST=coverage run -m pytest --capture=sys
+set PYTEST=pytest --capture=sys
 
 %PYTEST% -v -m "not requires_pufcsv and not pre_release and not local"
