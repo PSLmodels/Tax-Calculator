b99acf19b0c699bf08e048bdaffc514d903855bc
Martin Holmer <martin.holmer@gmail.com>
2019-01-26 19:08:16 -0500

    Rewrite AdditionalCTC function

--- taxcalc/calcfunctions.py
+++ taxcalc/calcfunctions.py
@@ -1226,7 +1226,7 @@ def EITC(MARS, DSI, EIC, c00100, e00300, e00400, e00600, c01000,
 
 
 @iterate_jit(nopython=True)
-def ChildDepTaxCredit(n24, MARS, c00100, XTOT, num, c05200,
+def ChildDepTaxCredit(n24, MARS, c00100, XTOT, num, c05800,
                       e07260, CR_ResidentialEnergy_hc,
                       e07300, CR_ForeignTax_hc,
                       c07180,
@@ -1236,10 +1236,10 @@ def ChildDepTaxCredit(n24, MARS, c00100, XTOT, num, c05200,
                       CTC_c, CTC_ps, CTC_prt, exact,
                       DependentCredit_Child_c, DependentCredit_Nonchild_c,
                       CTC_c_under5_bonus, nu05, FilerCredit_c,
-                      prectc, c07220):
+                      c07220, dep_credit, codtc_limited):
     """
     Computes amounts on "Child Tax Credit and Credit for Other Dependents
-    Worksheet" in 2018 Publication 972, which pretain to these two
+    Worksheet" in 2018 Publication 972, which pertain to these two
     nonrefundable tax credits.
     """
     # Worksheet Part 1
@@ -1258,7 +1258,7 @@ def ChildDepTaxCredit(n24, MARS, c00100, XTOT, num, c05200,
         line10 = line3
     if line10 > 0.:
         # Worksheet Part 2
-        line11 = c05200
+        line11 = c05800
         line12 = (e07260 * (1. - CR_ResidentialEnergy_hc) +
                   e07300 * (1. - CR_ForeignTax_hc) +
                   c07180 +  # child & dependent care expense credit
@@ -1271,12 +1271,16 @@ def ChildDepTaxCredit(n24, MARS, c00100, XTOT, num, c05200,
         line16 = min(line10, line15)  # credit is capped by tax liability
     else:
         line16 = 0.
-    if line1 > 0.:
-        prectc = line10  # post phaseout credit
-    else:
-        prectc = 0.
-    c07220 = line16  # combined nonrefundable CTC and ODTC amount
-    return (prectc, c07220)
+    # separate the CTC and ODTC amounts
+    c07220 = 0.  # nonrefundable CTC amount
+    dep_credit = 0.  # nonrefundable ODTC amount
+    if line16 > 0.:
+        if line1 > 0.:
+            c07220 = line16 * line1 / line3
+        dep_credit = max(0., line16 - c07220)
+    # compute codtc_limited for use in AdditionalCTC function
+    codtc_limited = max(0., line10 - line16)
+    return (c07220, dep_credit, codtc_limited)
 
 
 @iterate_jit(nopython=True)
@@ -1376,7 +1380,7 @@ def SchR(age_head, age_spouse, MARS, c00100,
     are not able to be changed using Policy class parameters.
 
     Note that the CR_SchR_hc policy parameter allows the user to eliminate
-    or reduce total schedule R credits.
+    or reduce total Schedule R credits.
     """
     if age_head >= 65 or (MARS == 2 and age_spouse >= 65):
         # calculate credit assuming nobody is disabled (so line12 = line10)
@@ -1496,7 +1500,7 @@ def CharityCredit(e19800, e20100, c00100, CR_Charity_rt, CR_Charity_f,
 
 @iterate_jit(nopython=True)
 def NonrefundableCredits(c05800, e07240, e07260, e07300, e07400,
-                         e07600, p08000, prectc, dep_credit,
+                         e07600, p08000, dep_credit,
                          personal_nonrefundable_credit,
                          CR_RetirementSavings_hc, CR_ForeignTax_hc,
                          CR_ResidentialEnergy_hc, CR_GeneralBusiness_hc,
@@ -1531,11 +1535,11 @@ def NonrefundableCredits(c05800, e07240, e07260, e07300, e07400,
     c07240 = min(e07240 * (1. - CR_RetirementSavings_hc), avail)
     avail = avail - c07240
     if DependentCredit_before_CTC:
-        # Dependent credit
+        # Other dependent credit
         dep_credit = min(dep_credit, avail)
         avail = avail - dep_credit
     # Child tax credit
-    c07220 = min(prectc, avail)
+    c07220 = min(c07220, avail)
     avail = avail - c07220
     # Residential energy credit - Form 5695
     c07260 = min(e07260 * (1. - CR_ResidentialEnergy_hc), avail)
@@ -1567,59 +1571,41 @@ def NonrefundableCredits(c05800, e07240, e07260, e07300, e07400,
 
 
 @iterate_jit(nopython=True)
-def AdditionalCTC(n24, prectc, earned, c07220, ptax_was,
-                  ACTC_Income_thd, ACTC_rt, ACTC_ChildNum,
-                  c03260, e09800, c59660, e11200, c11070, nu05,
-                  ACTC_rt_bonus_under5family):
+def AdditionalCTC(codtc_limited, CTC_c, n24, earned, ACTC_Income_thd,
+                  ACTC_rt, nu05, ACTC_rt_bonus_under5family, ACTC_ChildNum,
+                  ptax_was, c03260, e09800, c59660, e11200,
+                  c11070):
     """
-    Calculates Additional (refundable) Child Tax Credit, c11070.
+    Calculates refundable Additional Child Tax Credit (ACTC), c11070,
+    following 2018 Form 8812 logic.
     """
-    c82925 = 0.
-    c82930 = 0.
-    c82935 = 0.
-    c82880 = 0.
-    c82885 = 0.
-    c82890 = 0.
-    c82900 = 0.
-    c82905 = 0.
-    c82910 = 0.
-    c82915 = 0.
-    c82920 = 0.
-    c82937 = 0.
-    c82940 = 0.
-    c11070 = 0.
-    # Part I of 2005 Form 8812
-    if n24 > 0:
-        c82925 = prectc
-        c82930 = c07220
-        c82935 = c82925 - c82930
-        # CTC not applied to tax
-        c82880 = max(0., earned)
-        c82885 = max(0., c82880 - ACTC_Income_thd)
-        # Accomodate ACTC rate bonus for families with children under 5
+    # Part I
+    line3 = codtc_limited
+    line4 = CTC_c * n24
+    c11070 = 0.  # line15
+    if line3 > 0. and line4 > 0.:
+        line5 = min(line3, line4)
+        line7 = max(0., earned - ACTC_Income_thd)
+        # accommodate ACTC rate bonus for families with children under 5
         if nu05 == 0:
             ACTC_rate = ACTC_rt
         else:
             ACTC_rate = ACTC_rt + ACTC_rt_bonus_under5family
-        c82890 = ACTC_rate * c82885
-    # Part II of 2005 Form 8812
-    if n24 >= ACTC_ChildNum and c82890 < c82935:
-        c82900 = 0.5 * ptax_was
-        c82905 = c03260 + e09800
-        c82910 = c82900 + c82905
-        c82915 = c59660 + e11200
-        c82920 = max(0., c82910 - c82915)
-        c82937 = max(c82890, c82920)
-    # Part II of 2005 Form 8812
-    if n24 < ACTC_ChildNum:
-        if n24 > 0 and c82890 > 0:
-            c82940 = min(c82890, c82935)
-    else:  # if n24 >= ACTC_ChildNum
-        if c82890 >= c82935:
-            c82940 = c82935
-        else:
-            c82940 = min(c82935, c82937)
-    c11070 = c82940
+        line8 = ACTC_rate * line7
+        if n24 < ACTC_ChildNum:
+            if line8 > 0.:
+                c11070 = min(line5, line8)
+        else:  # if n24 >= ACTC_ChildNum
+            if line8 >= line5:
+                c11070 = line5
+            else:  # complete Part II
+                line9 = 0.5 * ptax_was
+                line10 = c03260 + e09800
+                line11 = line9 + line10
+                line12 = c59660 + e11200
+                line13 = max(0., line11 - line12)
+                line14 = max(line8, line13)
+                c11070 = min(line5, line14)
     return c11070
 
 
