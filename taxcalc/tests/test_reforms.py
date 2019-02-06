128bec1bc69204412021c1696711fe1fc343e1cb
Martin Holmer <martin.holmer@gmail.com>
2019-02-06 10:06:23 -0500

    Archive reforms that can no longer be simulated

--- taxcalc/tests/test_reforms.py
+++ taxcalc/tests/test_reforms.py
@@ -134,8 +134,7 @@ def test_round_trip_tcja_reform(tests_path):
     if fail_params:
         raise ValueError(msg)
 
-@pytest.mark.one
-@pytest.mark.pre_release
+
 def test_reform_json_and_output(tests_path):
     """
     Check that each JSON reform file can be converted into a reform dictionary
