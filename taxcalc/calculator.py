5ff2faffccd608b8da5ae1ae0344ed8a84003b02
Martin Holmer <martin.holmer@gmail.com>
2019-02-08 15:26:52 -0500

    Rename Calculator read_json_assumptions as read_json_parameters

--- taxcalc/calculator.py
+++ taxcalc/calculator.py
@@ -1141,40 +1141,42 @@ class Calculator():
         return param_dict
 
     @staticmethod
-    def read_json_assumptions(assump):
+    def read_json_parameters(param):
         """
-        Read JSON assump object and return one dictionary that has
+        Read JSON param object and return one dictionary that has
         the same structure as each subdictionary returned by the
         Calculator.read_json_param_objects method.
 
-        Note that the assump argument can be None, in which case
+        Note that the param argument can be None, in which case
         the returned dictionary is empty.  Also note that the
-        assump argument can be a string containing a local file name,
+        param argument can be a string containing a local file name,
         URL beginning with 'http', or valid JSON string.
 
-        The assump file/URL contents or JSON string must be like the
+        The param file/URL contents or JSON string must be like the
         structure required for each subsection of the assump argument
-        of the Calculator.read_json_param_objects method.
+        of the Calculator.read_json_param_objects method EXCEPT FOR
+        ONE THING: the parameter values are NOT enclosed in [] brackets.
         """
         # construct returned dictionary from specified assump
-        if assump is None:
+        if param is None:
             returned_dict = dict()
-        elif isinstance(assump, str):
-            if os.path.isfile(assump):
-                txt = open(assump, 'r').read()
-            elif assump.startswith('http'):
-                req = requests.get(assump)
+        elif isinstance(param, str):
+            if os.path.isfile(param):
+                txt = open(param, 'r').read()
+            elif param.startswith('http'):
+                req = requests.get(param)
                 req.raise_for_status()
                 txt = req.text
+                print(txt)
             else:
-                txt = assump
+                txt = param
             # strip out //-comments without changing line numbers
             json_text = re.sub('//.*', ' ', txt)
             # convert JSON text into a Python dictionary
             raw_dict = json_to_dict(json_text)
             returned_dict = Calculator._convert_parameter_dict(raw_dict)
         else:
-            raise ValueError('assump is neither None nor string')
+            raise ValueError('param is neither None nor string')
         return returned_dict
 
     @staticmethod
@@ -1583,15 +1585,15 @@ class Calculator():
         year_param = dict()
         for pkey, sdict in param_key_dict.items():
             if not isinstance(pkey, str):
-                msg = 'pkey {} in reform is not a string'
+                msg = 'pkey {} in param JSON is not a string'
                 raise ValueError(msg.format(pkey))
             rdict = dict()
             if not isinstance(sdict, dict):
-                msg = 'pkey {} in reform is not paired with a dict'
+                msg = 'pkey {} in param JSON is not paired with a dict'
                 raise ValueError(msg.format(pkey))
             for skey, val in sdict.items():
                 if not isinstance(skey, str):
-                    msg = 'skey {} in reform is not a string'
+                    msg = 'skey {} in param JSON is not a string'
                     raise ValueError(msg.format(skey))
                 else:
                     year = int(skey)
