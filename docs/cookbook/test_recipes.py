9596de2a717fe51766adaf0cee749559dc18ef28
Martin Holmer <martin.holmer@gmail.com>
2019-03-18 09:12:47 -0400

    Revise names of cookbook-related HTML files

--- docs/cookbook/test_recipes.py
+++ docs/cookbook/test_recipes.py
@@ -14,13 +14,14 @@ import difflib
 
 
 # print start time
-print('{}'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
 
 # make list of recipeNN.py filenames
 RECIPES = glob.glob('./recipe[0-9][0-9].py')
 
 # execute each recipe in RECIPES list and compare output with expected output
-for recipe in sorted(RECIPES):
+for recipe_path in sorted(RECIPES):
+    recipe = recipe_path.replace('./', '')
     out_filename = recipe.replace('.py', '.out')
     if os.path.isfile(out_filename):
         os.remove(out_filename)
@@ -29,7 +30,8 @@ for recipe in sorted(RECIPES):
     except subprocess.CalledProcessError as err:
         print('{} FAIL with error rtncode={}'.format(recipe, err.returncode))
         continue  # to next recipe
-    with open(recipe.replace('.py', '.res'), 'r') as resfile:
+    res_filename = recipe.replace('.py', '.res')
+    with open(res_filename, 'r') as resfile:
         exp = resfile.read()
     # check for differences between out and exp results
     actual = out.splitlines(True)
@@ -41,12 +43,12 @@ for recipe in sorted(RECIPES):
         diff_lines.append(line)
     # write actual output to file if any differences; else report PASS
     if diff_lines:
-        print('{} FAIL with output differences'.format(recipe))
-        outfilename = recipe.replace('.py', '.out')
-        with open(outfilename, 'w') as outfile:
+        with open(out_filename, 'w') as outfile:
             outfile.write(out)
+        msg = '{} FAIL : actual in {} & expected in {}'
+        print(msg.format(recipe, out_filename, res_filename))
     else:
         print('{} PASS'.format(recipe))
 
 # print finish time
-print('{}'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
