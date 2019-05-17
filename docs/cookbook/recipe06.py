c4dd4d265443c3422c82665ecdf83698b95dcf35
Martin Holmer <martin.holmer@gmail.com>
2019-05-17 19:15:05 -0400

    More development of cookbook/recipe06.py

--- docs/cookbook/recipe06.py
+++ docs/cookbook/recipe06.py
@@ -1,57 +1,30 @@
 import numpy as np
-import pandas as pd
 import taxcalc as tc
 
 
-# begin customizing tc.Calculator class methods and calcfunctions
+# begin adding/customizing tc.Calculator class methods and calcfunctions
 
-def specify_pseudo_COLR_policy(self):
-    """
-    Specifies policy parameters for the policy in the self.current_year
-    """
-    # reform implementation year
-    reform_year = 2020
-    # one dollar refundable credit for each dollar of earnings
-    self.__policy.setattr('colr_rt', 1.0)
-    # ceiling on refundable credit varies by type of filing unit
-    valuelist = [4000.0, 8000.0, 4000.0, 4000.0, 4000.0]
-    self.__policy.setattr('colr_c', np.array(valuelist, np.float64))
-    # AGI phase-out start threshold varies by type of filing unit
-    valuelist = [30000.0, 50000.0, 30000.0, 30000.0, 30000.0]
-    self.__policy.setattr('colr_ps', np.array(valuelist, np.float64))
-    # credit phase-out rate per dollar of AGI above phase-out start threshold
-    self.__policy.setattr('colr_prt', 0.20)
-    # set parameters for years other than reform_year
-    this_year = self.current_year
-    if self.colr_active and this_year >= reform_year:
-        # set inflation-indexed values of colr_c and colr_ps for this_year
-        irates = self.__policy.inflation_rates()
-        syr = Policy.JSON_START_YEAR
-        for param in ['colr_c', 'colr_ps']:
-            value = self.__policy.getattr(param)
-            for year in range(reform_year, this_year):
-                value *= (1.0 + irates[year - syr])
-            self.__policy.setattr(param, value)
-    else:
-        # set credit ceiling to zero
-        valuelist = [0.0, 0.0, 0.0, 0.0, 0.0]
-        self.__policy.setattr('colr_c', np.array(valuelist, np.float65))
+#                 COLR_rt, COLR_c, COLR_ps, COLR_prt,
 
 @tc.iterate_jit(nopython=True)
-def pseudo_COLR(e00200, MARS, col_refund, iitax, combined):
+def pseudo_COLR(e00200, MARS, c00100,
+                COLR_rt, COLR_c, COLR_ps, COLR_prt,
+                COLR_amount, iitax, combined):
     """
     Calculates pseudo Cost-of-Living Refund amount.
     Note this is simply meant to illustrate a Python programming technique;
     this function does NOT calculate the exact Cost-of-Living Refund amount.
+    See setting of COLR parameters below in specify_pseudo_COLR_policy method.
     """
     # calculate pseudo refund amount
-    col_refund = 1000.
+    COLR_amount = 1000.
     # reduce income & combined tax liability because it is a refundable credit
-    iitax -= col_refund
-    combined -= col_refund
-    return (col_refund, iitax, combined)
-    
+    iitax -= COLR_amount
+    combined -= COLR_amount
+    return (COLR_amount, iitax, combined)
+
 # end new calcfunctions used by customized Calculator class
+
 
 class Calculator(tc.Calculator):
     """
@@ -69,6 +42,39 @@ class Calculator(tc.Calculator):
         # remember whether pseudo_COLR is active or not
         self.colr_active = colr_active
 
+    def specify_pseudo_COLR_policy(self):
+        """
+        Specifies policy parameters for the COLR policy in the current_year.
+        See use of these parameters above in the pseudo_COLR calcfunction.
+        """
+        # reform implementation year
+        reform_year = 2020
+        # one dollar refundable credit for each dollar of earnings
+        setattr(self.__policy, 'COLR_rt', np.array([1.0], np.float64))
+        # ceiling on refundable credit varies by type of filing unit
+        valuelist = [4000.0, 8000.0, 4000.0, 4000.0, 4000.0]
+        setattr(self.__policy, 'COLR_c', np.array(valuelist, np.float64))
+        # AGI phase-out start threshold varies by type of filing unit
+        valuelist = [30000.0, 50000.0, 30000.0, 30000.0, 30000.0]
+        setattr(self.__policy, 'COLR_ps', np.array(valuelist, np.float64))
+        # credit phase-out rate per dollar of AGI above phase-out start level
+        setattr(self.__policy, 'COLR_prt', np.array([0.2], np.float64))
+        # set parameters for years other than reform_year
+        this_year = self.current_year
+        if self.colr_active and this_year >= reform_year:
+            # set inflation-indexed values of COLR_c and COLR_ps for this_year
+            irates = self.__policy.inflation_rates()
+            syr = tc.Policy.JSON_START_YEAR
+            for param in ['COLR_c', 'COLR_ps']:
+                value = getattr(self.__policy, param)
+                for year in range(reform_year, this_year):
+                    value *= (1.0 + irates[year - syr])
+                setattr(self.__policy, param, value)
+        else:
+            # set credit ceiling to zero
+            valuelist = [0.0, 0.0, 0.0, 0.0, 0.0]
+            setattr(self.__policy, 'COLR_c', np.array(valuelist, np.float64))
+
     def calc_all(self, zero_out_calc_vars=False):
         """
         Call all tax-calculation functions for the current_year.
@@ -81,9 +87,9 @@ class Calculator(tc.Calculator):
         tc.LumpSumTax(self.__policy, self.__records)
         # specify new Records variable to hold pseudo COLR amount
         zeros = np.zeros(self.__records.array_length, dtype=np.float64)
-        setattr(self.__records, 'col_refund', zeros)
+        setattr(self.__records, 'COLR_amount', zeros)
         # specify new Policy parameters to characterize pseudo COLR policy
-        
+        self.specify_pseudo_COLR_policy()  # (see above)
         # call new function that calculates pseudo COLRefund amount
         pseudo_COLR(self.__policy, self.__records)  # (see above)
         tc.ExpandIncome(self.__policy, self.__records)
@@ -91,8 +97,10 @@ class Calculator(tc.Calculator):
 
 # end of customized Calculator class definition
 
+
 # top-level logic of program that uses customized Calculator class
 
+"""
 # read an "old" reform file from Tax-Calculator website
 # ("old" means the reform file is defined relative to pre-TCJA policy)
 reforms_url = ('https://raw.githubusercontent.com/'
@@ -115,13 +123,17 @@ rpolicy.implement_reform(reform1, print_warnings=False, raise_errors=False)
 assert not rpolicy.parameter_errors
 rpolicy.implement_reform(reform2, print_warnings=False, raise_errors=False)
 assert not rpolicy.parameter_errors
+"""
+
+bpolicy = tc.Policy()
+rpolicy = tc.Policy()
 
 # specify customized Calculator objects using bpolicy and rpolicy
 recs = tc.Records.cps_constructor()
-calc1 = Calculator(policy=bpolicy, records=recs)
-calc2 = Calculator(policy=rpolicy, records=recs)
+calc1 = Calculator(policy=bpolicy, records=recs, colr_active=False)
+calc2 = Calculator(policy=rpolicy, records=recs, colr_active=True)
 
-cyr = 2018
+cyr = 2018   # was 2018
 
 # calculate for specified cyr
 calc1.advance_to_year(cyr)
@@ -130,11 +142,13 @@ calc2.advance_to_year(cyr)
 calc2.calc_all()
 
 # compare aggregate individual income tax revenue in cyr
-iitax_rev1 = calc1.weighted_total('iitax')
-iitax_rev2 = calc2.weighted_total('iitax')
+iitax_funits = calc1.total_weight()
+iitax_reven1 = calc1.weighted_total('iitax')
+iitax_reven2 = calc2.weighted_total('iitax')
 
 # print total revenue estimates for cyr
-# (estimates in billons of dollars)
-print('{}_REFORM1_iitax_rev($B)= {:.3f}'.format(cyr, iitax_rev1 * 1e-9))
-print('{}_REFORM2_iitax_rev($B)= {:.3f}'.format(cyr, iitax_rev2 * 1e-9))
+# (estimates in millions of people and billons of dollars)
+print('{}_FUNITS(#M)= {:.3f}'.format(cyr, iitax_funits * 1e-6))
+print('{}_BASELN_iitax_rev($B)= {:.3f}'.format(cyr, iitax_reven1 * 1e-9))
+print('{}_REFORM_iitax_rev($B)= {:.3f}'.format(cyr, iitax_reven2 * 1e-9))
 print('')
