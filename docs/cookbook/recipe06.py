445d7b1a5c4623c700436cb6e66c69a080cf78bd
Martin Holmer <martin.holmer@gmail.com>
2019-05-18 15:05:08 -0400

    Check specify_pseudo_COLR_policy() logic

--- docs/cookbook/recipe06.py
+++ docs/cookbook/recipe06.py
@@ -4,7 +4,6 @@ import taxcalc as tc
 
 # begin adding/customizing tc.Calculator class methods and calcfunctions
 
-#                 COLR_rt, COLR_c, COLR_ps, COLR_prt,
 
 @tc.iterate_jit(nopython=True)
 def pseudo_COLR(e00200, MARS, c00100,
@@ -49,31 +48,38 @@ class Calculator(tc.Calculator):
         """
         # reform implementation year
         reform_year = 2020
-        # one dollar refundable credit for each dollar of earnings
-        setattr(self.__policy, 'COLR_rt', np.array([1.0], np.float64))
-        # ceiling on refundable credit varies by type of filing unit
-        valuelist = [4000.0, 8000.0, 4000.0, 4000.0, 4000.0]
-        setattr(self.__policy, 'COLR_c', np.array(valuelist, np.float64))
-        # AGI phase-out start threshold varies by type of filing unit
-        valuelist = [30000.0, 50000.0, 30000.0, 30000.0, 30000.0]
-        setattr(self.__policy, 'COLR_ps', np.array(valuelist, np.float64))
-        # credit phase-out rate per dollar of AGI above phase-out start level
-        setattr(self.__policy, 'COLR_prt', np.array([0.2], np.float64))
-        # set parameters for years other than reform_year
+        # specify dictionary of parameter names and values for reform_year
+        pvalue = {
+            # credit phase-in rate on earnings
+            'COLR_rt': [1.0],
+            # ceiling on refundable credit varies by filing-unit type, MARS
+            'COLR_c': [4000.0, 8000.0, 4000.0, 4000.0, 4000.0],
+            # credit phase-out start AGI level varies by filing-unit type, MARS
+            'COLR_ps': [30000.0, 50000.0, 30000.0, 30000.0, 30000.0],
+            # credit phase-out rate per dollar of AGI above COLR_ps level
+            'COLR_prt': [0.2]
+        }
+        for name in pvalue:
+            setattr(self.__policy, name, np.array(pvalue[name], np.float64))
+        # set parameter values for self.current_year
         this_year = self.current_year
         if self.colr_active and this_year >= reform_year:
             # set inflation-indexed values of COLR_c and COLR_ps for this_year
             irates = self.__policy.inflation_rates()
             syr = tc.Policy.JSON_START_YEAR
-            for param in ['COLR_c', 'COLR_ps']:
-                value = getattr(self.__policy, param)
+            for name in ['COLR_c', 'COLR_ps']:
+                value = getattr(self.__policy, name)
                 for year in range(reform_year, this_year):
                     value *= (1.0 + irates[year - syr])
-                setattr(self.__policy, param, value)
+                setattr(self.__policy, name, value)
         else:
-            # set credit ceiling to zero
-            valuelist = [0.0, 0.0, 0.0, 0.0, 0.0]
-            setattr(self.__policy, 'COLR_c', np.array(valuelist, np.float64))
+            # set pseudo COLR ceiling amount to zero
+            zeros = [0.0, 0.0, 0.0, 0.0, 0.0]
+            setattr(self.__policy, 'COLR_c', np.array(zeros, np.float64))
+        for name in pvalue:
+            value = getattr(self.__policy, name)
+            for idx in range(0, len(value)):
+                print('> {} {} {:.2f}'.format(this_year, name, value[idx]))
 
     def calc_all(self, zero_out_calc_vars=False):
         """
@@ -133,22 +139,23 @@ recs = tc.Records.cps_constructor()
 calc1 = Calculator(policy=bpolicy, records=recs, colr_active=False)
 calc2 = Calculator(policy=rpolicy, records=recs, colr_active=True)
 
-cyr = 2018   # was 2018
-
-# calculate for specified cyr
-calc1.advance_to_year(cyr)
-calc1.calc_all()
-calc2.advance_to_year(cyr)
-calc2.calc_all()
-
-# compare aggregate individual income tax revenue in cyr
-iitax_funits = calc1.total_weight()
-iitax_reven1 = calc1.weighted_total('iitax')
-iitax_reven2 = calc2.weighted_total('iitax')
-
-# print total revenue estimates for cyr
-# (estimates in millions of people and billons of dollars)
-print('{}_FUNITS(#M)= {:.3f}'.format(cyr, iitax_funits * 1e-6))
-print('{}_BASELN_iitax_rev($B)= {:.3f}'.format(cyr, iitax_reven1 * 1e-9))
-print('{}_REFORM_iitax_rev($B)= {:.3f}'.format(cyr, iitax_reven2 * 1e-9))
-print('')
+for cyr in range(2018, 2022 + 1):
+    # advance to and calculate for specified cyr
+    calc1.advance_to_year(cyr)
+    calc1.calc_all()
+    calc2.advance_to_year(cyr)
+    calc2.calc_all()
+    # extract weighted amounts
+    iitax_funits = calc1.total_weight()
+    iitax_reven1 = calc1.weighted_total('iitax')
+    iitax_reven2 = calc2.weighted_total('iitax')
+    colr_amount = calc2.weighted_total('COLR_amount')
+    # print weighted amounts for cyr
+    line = 'YEAR,NUM,ITAX1,ITAX2,COLR=  {}  {:.3f}  {:.3f}  {:.3f}  {:.3f}'
+    print(line.format(
+        cyr,
+        iitax_funits * 1e-6,
+        iitax_reven1 * 1e-9,
+        iitax_reven2 * 1e-9,
+        colr_amount * 1e-9
+    ))
