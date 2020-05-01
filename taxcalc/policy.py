65f5083705aaa636398b64a1b6d3001b100cb2e5
hdoupe <henrymdoupe@gmail.com>
2020-05-01 11:00:28 -0400

    Updates for forthcoming paramtools release - Adds back expand_array tests - Use to_array labels argument in '_{attr}' logic - Move initialize logic to __init__ method but   keep initialize for legacy purposes

--- taxcalc/policy.py
+++ taxcalc/policy.py
@@ -73,7 +73,7 @@ class Policy(Parameters):
     # (3) specify which Policy parameters are wage (rather than price) indexed
     WAGE_INDEXED_PARAMS = ['SS_Earnings_c', 'SS_Earnings_thd']
 
-    def __init__(self, gfactors=None, only_reading_defaults=False):
+    def __init__(self, gfactors=None, only_reading_defaults=False, **kwargs):
         # put JSON contents of DEFAULTS_FILE_NAME into self._vals dictionary
         super().__init__()
         # handle gfactors argument
@@ -92,7 +92,7 @@ class Policy(Parameters):
         self.initialize(syr, nyrs, Policy.LAST_KNOWN_YEAR,
                         Policy.REMOVED_PARAMS,
                         Policy.REDEFINED_PARAMS,
-                        Policy.WAGE_INDEXED_PARAMS)
+                        Policy.WAGE_INDEXED_PARAMS, **kwargs)
 
     @staticmethod
     def read_json_reform(obj):
