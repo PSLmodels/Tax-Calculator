75bf2c9199e6d840cc46cbe29f1ec752a0cf1ef3
Max Ghenis <mghenis@gmail.com>
2020-06-26 12:40:52 -0700

    Update recipe00 using jupytext

--- docs/jupyter_src/convert_all.sh
+++ docs/jupyter_src/convert_all.sh
@@ -1,3 +1,4 @@
+jupytext recipe00.ipynb --to myst -o ../jb/recipes/recipe00.md
 jupytext recipe01.ipynb --to myst -o ../jb/recipes/recipe01.md
 jupytext recipe02.ipynb --to myst -o ../jb/recipes/recipe02.md
 jupytext recipe03.ipynb --to myst -o ../jb/recipes/recipe03.md
