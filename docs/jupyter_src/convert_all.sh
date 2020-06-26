e8c5cd4bfb071d821c5529828b06ee532cd9f019
Max Ghenis <mghenis@gmail.com>
2020-06-26 12:21:59 -0700

    Build jupyterbook

--- docs/jupyter_src/convert_all.sh
+++ docs/jupyter_src/convert_all.sh
@@ -1,7 +1,7 @@
-jupytext recipe01.ipynb --to myst -o ../jb/recipe01.md
-jupytext recipe02.ipynb --to myst -o ../jb/recipe02.md
-jupytext recipe03.ipynb --to myst -o ../jb/recipe03.md
-jupytext recipe04.ipynb --to myst -o ../jb/recipe04.md
-jupytext recipe04_pandas.ipynb --to myst -o ../jb/recipe04_pandas.md
-jupytext recipe05.ipynb --to myst -o ../jb/recipe05.md
-jupytext recipe06.ipynb --to myst -o ../jb/recipe06.md
+jupytext recipe01.ipynb --to myst -o ../jb/recipes/recipe01.md
+jupytext recipe02.ipynb --to myst -o ../jb/recipes/recipe02.md
+jupytext recipe03.ipynb --to myst -o ../jb/recipes/recipe03.md
+jupytext recipe04.ipynb --to myst -o ../jb/recipes/recipe04.md
+jupytext recipe04_pandas.ipynb --to myst -o ../jb/recipes/recipe04_pandas.md
+jupytext recipe05.ipynb --to myst -o ../jb/recipes/recipe05.md
+jupytext recipe06.ipynb --to myst -o ../jb/recipes/recipe06.md
