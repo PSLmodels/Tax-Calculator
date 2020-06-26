ad09f3b0c1fbeeaeb33f92ce0524027b28f909db
Max Ghenis <mghenis@gmail.com>
2020-06-26 11:10:33 -0700

    Add ipynb files from previous jupinx work and convert them to MyST via jupytext

---
+++ docs/jb/recipes/jupyter_src/README.md
@@ -0,0 +1,6 @@
+Jupyter notebooks used to convert to MyST via Jupytext
+======================================================
+
+Per https://jupyterbook.org/content-types/myst-notebooks.html#create-a-myst-notebook-with-jupytext.
+
+To convert all and replace the MyST files in the parent directory, run `. convert_all.sh`.
---
+++ docs/jb/recipes/jupyter_src/convert_all.sh
@@ -0,0 +1,7 @@
+jupytext recipe01.ipynb --to myst -o ../recipe01.md
+jupytext recipe02.ipynb --to myst -o ../recipe02.md
+jupytext recipe03.ipynb --to myst -o ../recipe03.md
+jupytext recipe04.ipynb --to myst -o ../recipe04.md
+jupytext recipe04_pandas.ipynb --to myst -o ../recipe04_pandas.md
+jupytext recipe05.ipynb --to myst -o ../recipe05.md
+jupytext recipe06.ipynb --to myst -o ../recipe06.md
