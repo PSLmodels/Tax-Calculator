540407c24ba374119c10a4b07ce743ba8f28ed10
Max Ghenis <mghenis@gmail.com>
2020-07-05 23:07:45 -0700

    Switch ipynb and md recipes so they can open in colab

--- docs/recipes/jupyter_src/convert_all.sh
+++
@@ -1,8 +0,0 @@
-jupytext recipe00.ipynb --to myst -o ../jb/recipes/recipe00.md
-jupytext recipe01.ipynb --to myst -o ../jb/recipes/recipe01.md
-jupytext recipe02.ipynb --to myst -o ../jb/recipes/recipe02.md
-jupytext recipe03.ipynb --to myst -o ../jb/recipes/recipe03.md
-jupytext recipe04.ipynb --to myst -o ../jb/recipes/recipe04.md
-jupytext recipe04_pandas.ipynb --to myst -o ../jb/recipes/recipe04_pandas.md
-jupytext recipe05.ipynb --to myst -o ../jb/recipes/recipe05.md
-jupytext recipe06.ipynb --to myst -o ../jb/recipes/recipe06.md
