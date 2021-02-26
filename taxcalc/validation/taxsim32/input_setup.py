fc2d01159c54d06f559ec9d35dad53c75a9c029c
Jacob C <44846403+chusloj@users.noreply.github.com>
2021-02-26 13:50:36 -0500

    updated formatting for main comparison files

--- taxcalc/validation/taxsim32/input_setup.py
+++ taxcalc/validation/taxsim32/input_setup.py
@@ -12,13 +12,13 @@ def get_inputs():
     """
     Runs taxsim_input.py for all combinations of year and assumption sets
     """
-    letters = ['a', 'b', 'c']
-    years = ['2018', '2019']
+    letters = ["a", "b", "c"]
+    years = ["2018", "2019"]
 
-    name_list = [str(y + ' ' + x) for x in letters for y in years]
+    name_list = [str(y + " " + x) for x in letters for y in years]
 
     for name in name_list:
-        command = str('python taxsim_input.py ' + name)
+        command = str("python taxsim_input.py " + name)
         os.system(command)
 
 
@@ -27,30 +27,32 @@ def get_ftp_output():
     Uses `curl` to upload assumption set input files
     and save taxsim-32 output files
     """
-    letters = ['a', 'b', 'c']
-    years = ['18', '19']
-    file_list = [str(x+y + '.in') for x in letters for y in years]
+    letters = ["a", "b", "c"]
+    years = ["18", "19"]
+    file_list = [str(x + y + ".in") for x in letters for y in years]
 
     for f in file_list:
-        file_out = f + '.out-taxsim'
-        os.system(
-            f'curl -u taxsim:02138 -T {f} ftp://taxsimftp.nber.org/tmp/userid')
-        c_out = str('curl -u taxsim:02138 ' +
-                    'ftp://taxsimftp.nber.org/tmp/userid.txm32 -o ' + file_out)
+        file_out = f + ".out-taxsim"
+        os.system(f"curl -u taxsim:02138 -T {f} ftp://taxsimftp.nber.org/tmp/userid")
+        c_out = str(
+            "curl -u taxsim:02138 "
+            + "ftp://taxsimftp.nber.org/tmp/userid.txm32 -o "
+            + file_out
+        )
         os.system(c_out)
 
 
 def change_delim():
     for file in glob.glob("*.in.out-taxsim"):
         # Read in the file
-        with open(file, 'r') as fin:
+        with open(file, "r") as fin:
             filedata = fin.read()
 
         # Replace the target string
-        filedata = filedata.replace(',', ' ')
+        filedata = filedata.replace(",", " ")
 
         # Write the file out again
-        with open(file, 'w') as fout:
+        with open(file, "w") as fout:
             fout.write(filedata)
 
 
--- taxcalc/validation/taxsim32/main_comparison.py
+++ taxcalc/validation/taxsim32/main_comparison.py
@@ -8,64 +8,80 @@ import pandas as pd
 
 
 def main(assump_set, year):
-    
+
     # (1) generate TAXSIM-32-formatted output using Tax-Calculator tc CLI
-    os.system(f'python taxcalc.py {assump_set}{year}.in')
+    os.system(f"python taxcalc.py {assump_set}{year}.in")
 
     # (2) generate tax differences
-    taxsim_df = pd.read_csv(f'{assump_set}{year}.in.out-taxsim', sep=' ', skipinitialspace=True, index_col=False)
+    taxsim_df = pd.read_csv(
+        f"{assump_set}{year}.in.out-taxsim",
+        sep=" ",
+        skipinitialspace=True,
+        index_col=False,
+    )
     taxsim_df = taxsim_df.iloc[:, 0:28]
-    taxcalc_df = pd.read_csv(f'{assump_set}{year}.in.out-taxcalc', sep=' ', skipinitialspace=True, index_col=False, header=None)
-    
-    taxcalc_df.columns = taxsim_df.columns # rename taxcalc output columns
+    taxcalc_df = pd.read_csv(
+        f"{assump_set}{year}.in.out-taxcalc",
+        sep=" ",
+        skipinitialspace=True,
+        index_col=False,
+        header=None,
+    )
+
+    taxcalc_df.columns = taxsim_df.columns  # rename taxcalc output columns
 
     diff_dict = {
-                '# of differing records': [], 
-                 'max_diff': [],
-                 'max_diff_index': [],
-                 'max_diff_taxsim_val': [],
-                 'max_diff_taxcalc_val': []
-                 }
+        "# of differing records": [],
+        "max_diff": [],
+        "max_diff_index": [],
+        "max_diff_taxsim_val": [],
+        "max_diff_taxcalc_val": [],
+    }
 
     for col in taxsim_df.columns[3:]:
 
-        df_diff = pd.DataFrame({'a': taxsim_df[col], 'b': taxcalc_df[col]})
-        df_diff_recs = df_diff[df_diff['a'] != df_diff['b']]
-        diff_dict['# of differing records'].append(df_diff_recs.shape[0])
+        df_diff = pd.DataFrame({"a": taxsim_df[col], "b": taxcalc_df[col]})
+        df_diff_recs = df_diff[df_diff["a"] != df_diff["b"]]
+        diff_dict["# of differing records"].append(df_diff_recs.shape[0])
 
-        ind, max_val = max( enumerate( abs(x-y) for x,y in zip(taxcalc_df.loc[:, col], taxsim_df.loc[:, col]) ), key=lambda x: x[1] ) 
+        ind, max_val = max(
+            enumerate(
+                abs(x - y)
+                for x, y in zip(taxcalc_df.loc[:, col], taxsim_df.loc[:, col])
+            ),
+            key=lambda x: x[1],
+        )
 
-        diff_dict['max_diff'].append(taxcalc_df.loc[ind, col] - taxsim_df.loc[ind, col])
+        diff_dict["max_diff"].append(taxcalc_df.loc[ind, col] - taxsim_df.loc[ind, col])
         if max_val != 0:
-            diff_dict['max_diff_index'].append(ind)
-            diff_dict['max_diff_taxsim_val'].append(taxsim_df.loc[ind, col])
-            diff_dict['max_diff_taxcalc_val'].append(taxcalc_df.loc[ind, col])
+            diff_dict["max_diff_index"].append(ind)
+            diff_dict["max_diff_taxsim_val"].append(taxsim_df.loc[ind, col])
+            diff_dict["max_diff_taxcalc_val"].append(taxcalc_df.loc[ind, col])
         else:
-            diff_dict['max_diff_index'].append('no diff')
-            diff_dict['max_diff_taxsim_val'].append('no diff')
-            diff_dict['max_diff_taxcalc_val'].append('no diff')
-
+            diff_dict["max_diff_index"].append("no diff")
+            diff_dict["max_diff_taxsim_val"].append("no diff")
+            diff_dict["max_diff_taxcalc_val"].append("no diff")
 
     actual_df = pd.DataFrame(diff_dict, index=taxsim_df.columns[3:])
     print(f"Difference in dataframes for assumption set {assump_set} in year {year}")
     print(actual_df)
 
     # (3) check for difference between LYY.taxdiffs-actual and LYY.taxdiffs-expect
-    if os.path.isfile(f'{assump_set}{year}-taxdiffs-expect.csv'):
-        expect_df = pd.read_csv(f'{assump_set}{year}-taxdiffs-expect.csv', index_col=0)
-        
+    if os.path.isfile(f"{assump_set}{year}-taxdiffs-expect.csv"):
+        expect_df = pd.read_csv(f"{assump_set}{year}-taxdiffs-expect.csv", index_col=0)
+
         print(actual_df.eq(expect_df))
 
-        print('Above, True values mean the element is the same between the ACTUAL and EXPECT dataframes. ' +
-            '(EXPECT files are used for debugging purposes.)')
+        print(
+            "Above, True values mean the element is the same between the ACTUAL and EXPECT dataframes. "
+            + "(EXPECT files are used for debugging purposes.)"
+        )
     else:
         print("This EXPECT file doesn't exist.")
 
     # (4) Write the created df to *.taxdiffs-actual
-    actual_df.to_csv(f'{assump_set}{year}-taxdiffs-actual.csv')
+    actual_df.to_csv(f"{assump_set}{year}-taxdiffs-actual.csv")
 
 
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(main())
