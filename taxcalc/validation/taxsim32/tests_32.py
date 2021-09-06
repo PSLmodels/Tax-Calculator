db681e8590e7217259c42906f1edcae89bb53a79
jdebacker <jason.debacker@gmail.com>
2021-09-06 14:58:24 -0400

    have clean up only delete taxcalc files

--- taxcalc/validation/taxsim32/tests_32.py
+++ taxcalc/validation/taxsim32/tests_32.py
@@ -11,11 +11,12 @@ if not glob.glob(os.path.join(CUR_PATH, '*in.out-taxsim')):
 
 # run taxcalc/taxsim comparison
 for assump_set in ('a', 'b', 'c'):
-    for year in (18, 19):
+    for year in (17, 18, 19):
         main_comparison.main(assump_set, year)
 
-# clean up all files except those retrieved from TAXSIM32
+# clean up taxcalc files
+# keep taxsim files to avoid download again
 for file in CUR_PATH:
     for file in glob.glob('*.out*') and glob.glob('*.in*'):
-        if not file.endswith('taxsim'):
+        if file.endswith('taxcalc'):
             os.remove(file)
