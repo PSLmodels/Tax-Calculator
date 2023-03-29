a5f5ee94813034b3ac6312ebe5a16c4084e40c29
jdebacker <jason.debacker@gmail.com>
2023-03-29 11:19:14 -0400

    streamline var updates

--- taxcalc/validation/taxsim35/prepare_taxcalc_input.py
+++ taxcalc/validation/taxsim35/prepare_taxcalc_input.py
@@ -12,6 +12,43 @@ import numpy as np
 import pandas as pd
 
 
+TAXSIM_TC_MAP = {
+    'taxsimid': 'RECID',
+    'year': 'FLPDYR',
+    # 'state': # no Tax-Calculator use of TAXSIM variable 3, state code
+    # 'mstat', # Tax-Calculator MARS differs from TAXSIM mstat
+    'page': 'age_head',
+    'sage': 'age_spouse',
+    # 'depx',  # no direct Tax-Calculator use of number dependents
+    'dep13': 'f2441',
+    'dep17': 'n24',
+    # 'dep18': ' # no direct Tax-Calculator use of number EIC qualified dependents
+    'pwages': 'e00200p',
+    'swages': 'e00200s',
+    'dividends': 'e00650',
+    'intrec': 'e00300',
+    'stcg': 'p22250',
+    'ltcg': 'p23250',
+    'otherprop': 'e02000',
+    'nonprop': 'e00800',
+    'pensions': 'e01700',
+    'gssi': 'e02400',
+    'ui': 'e02300',
+    # 'transfers': # no Tax-Calculator use of TAXSIM variable 22, non-taxable transfers
+    # 'rentpaid' # no Tax-Calculator use of TAXSIM variable 23, rent paid
+    'proptax': 'e18500',
+    'otheritem': 'e18400',
+    'childcare': 'e32800',
+    'mortgage': 'e19200',
+    'scorp': 'e26270',
+    'pbusinc': 'e00900p',
+    # 'pprofinc': # no direct analog in Tax-Calculator
+    'sbusinc': 'e00900s',
+    # 'sprofinc':# no direct analog in Tax-Calculator
+
+}
+
+
 def main(file_in_name, file_out_name):
     """
     Translates TAXSIM-35 input file into a Tax-Calculator CSV-formatted
@@ -40,9 +77,9 @@ def main(file_in_name, file_out_name):
         os.remove(file_out_name)
     # read TAXSIM-35 INPUT file into a pandas DataFrame
     ivar = pd.read_csv(file_in_name, delim_whitespace=True,
-                       header=0, index_col=False, names=range(1, 33))
+                       header=0, index_col=False)
     # Drop 'idtl' – used to generate detailed output
-    ivar.drop(columns=32)
+    ivar.drop(columns=['idtl'], inplace=True)
     # translate INPUT variables into OUTPUT variables
     invar = translate(ivar)
     # write OUTPUT file containing Tax-Calculator input variables
@@ -58,55 +95,30 @@ def translate(ivar):
     Both ivar and returned invar are pandas DataFrame objects.
     """
     assert isinstance(ivar, pd.DataFrame)
-    invar = pd.DataFrame()
-    invar['RECID'] = ivar.loc[:, 1]
-    invar['FLPDYR'] = ivar.loc[:, 2]
-    # no Tax-Calculator use of TAXSIM variable 3, state code
-    mstat = ivar.loc[:, 4]
+    # Rename variables to be consistent with Tax-Calculator naming conventions
+    invar = ivar.rename(TAXSIM_TC_MAP, axis=1)
+    # Create variables for Tax-Calculator that aren't directly represented in TAXSIM
+    mstat = ivar['mstat']
     assert np.all(np.logical_or(mstat == 1, mstat == 2))
-    invar['age_head'] = ivar.loc[:, 5]
-    invar['age_spouse'] = ivar.loc[:, 6]
-    num_deps = ivar.loc[:, 7]
+    num_deps = ivar['depx']
     mars = np.where(mstat == 1, np.where(num_deps > 0, 4, 1), 2)
     assert np.all(np.logical_or(mars == 1,
                                 np.logical_or(mars == 2, mars == 4)))
     invar['MARS'] = mars
-    invar['f2441'] = ivar.loc[:, 8]
-    invar['n24'] = ivar.loc[:, 9]
-    num_eitc_qualified_kids = ivar.loc[:, 10]
+    num_eitc_qualified_kids = ivar['dep18']
     invar['EIC'] = np.minimum(num_eitc_qualified_kids, 3)
     num_taxpayers = np.where(mars == 2, 2, 1)
     invar['XTOT'] = num_taxpayers + num_deps
-    invar['e00200p'] = ivar.loc[:, 11]
-    invar['e00200s'] = ivar.loc[:, 12]
     invar['e00200'] = invar['e00200p'] + invar['e00200s']
-    invar['e00650'] = ivar.loc[:, 13]
     invar['e00600'] = invar['e00650']
-    invar['e00300'] = ivar.loc[:, 14]
-    invar['p22250'] = ivar.loc[:, 15]
-    invar['p23250'] = ivar.loc[:, 16]
-    invar['e02000'] = ivar.loc[:, 17]
-    invar['e00800'] = ivar.loc[:, 18]
-    invar['e01700'] = ivar.loc[:, 19]
     invar['e01500'] = invar['e01700']
-    invar['e02400'] = ivar.loc[:, 20]
-    invar['e02300'] = ivar.loc[:, 21]
-    # no Tax-Calculator use of TAXSIM variable 22, non-taxable transfers
-    # no Tax-Calculator use of TAXSIM variable 23, rent paid
-    invar['e18500'] = ivar.loc[:, 24]
-    invar['e18400'] = ivar.loc[:, 25]
-    invar['e32800'] = ivar.loc[:, 26]
-    invar['e19200'] = ivar.loc[:, 27]
-    invar['e26270'] = ivar.loc[:, 28]
-    invar['e00900p'] = ivar.loc[:, 29]
-    invar['e00900s'] = ivar.loc[:, 31]
     invar['e00900'] = invar['e00900p'] + invar['e00900s']
-
-    pprofinc = ivar.loc[:, 30]
-    sprofinc = ivar.loc[:, 32]
-
-    invar['PT_SSTB_income'] = 0#np.where(pprofinc + sprofinc > 0, 1, 0)
+    # variables for QBID calculation
+    invar['PT_SSTB_income'] = 0#np.where(invar['pprofinc'] + invar['sprofinc'] > 0, 1, 0)
     invar['PT_SSTB_income'] = 0#np.where(invar['e26270'] > 0, 1, invar['PT_SSTB_income'])
+
+    # Drop TAXSIM variables not used in Tax-Calculator
+    invar.drop(columns=['state', 'mstat', 'depx', 'dep18', 'transfers', 'rentpaid', 'pprofinc', 'sprofinc'], inplace=True)
 
     return invar
 
