7e93fd657626c2289e38e970f695fb0d4e85fb3d
martin.holmer@gmail.com <martin.holmer@gmail.com>
2024-09-10 09:41:38 -0400

    Upgrade to paramtools 0.18.3 version

--- setup.py
+++ setup.py
@@ -34,7 +34,6 @@ config = {
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
