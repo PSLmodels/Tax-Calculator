49e88fff1205d968e026c5de0b13ea5da8c61288
jdebacker <jason.debacker@gmail.com>
2025-03-10 17:27:54 -0400

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 with open("README.md", "r", encoding="utf-8") as f:
     longdesc = f.read()
 
-VERSION = "4.4.1"
+VERSION = "4.5.0"
 
 config = {
     "description": "Tax-Calculator",
