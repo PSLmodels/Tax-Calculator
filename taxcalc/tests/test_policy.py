1ccb25b39afb2869bc57697df6fc14fb4537264f
martin.holmer@gmail.com <martin.holmer@gmail.com>
2025-05-17 13:42:08 -0400

    Reduce use of pylint: disable= in test_parameters.py and test_policy.py

--- taxcalc/tests/test_policy.py
+++ taxcalc/tests/test_policy.py
@@ -12,7 +12,7 @@ import os
 import json
 import numpy as np
 import pytest
-import paramtools as pt
+import paramtools
 from taxcalc.policy import Policy
 
 
@@ -45,21 +45,20 @@ def test_incorrect_class_instantiation():
         Policy(gfactors=[])
 
 
-def test_correct_class_instantiation():
+def test_incorrect_implement_reform_usage():
     """
-    Test correct instantiation of Policy class object.
+    Test for incorrect implement_reform usage.
     """
     pol = Policy()
     assert pol
-    pol.implement_reform({})
-    with pytest.raises(pt.ValidationError):
+    with pytest.raises(paramtools.ValidationError):
         pol.implement_reform([])
-    with pytest.raises(pt.ValidationError):
+    with pytest.raises(paramtools.ValidationError):
         pol.implement_reform({2099: {'II_em': 99000}})
     pol.set_year(2019)
-    with pytest.raises(pt.ValidationError):
+    with pytest.raises(paramtools.ValidationError):
         pol.implement_reform({2018: {'II_em': 99000}})
-    with pytest.raises(pt.ValidationError):
+    with pytest.raises(paramtools.ValidationError):
         pol.implement_reform({2020: {'II_em': -1000}})
 
 
@@ -142,7 +141,7 @@ REFORM_JSON = """
 """
 
 
-# pylint: disable=protected-access,no-member
+# pylint: disable=protected-access
 
 
 @pytest.mark.parametrize("set_year", [False, True])
@@ -156,7 +155,6 @@ def test_read_json_reform_file_and_implement_reform(set_year):
         pol.set_year(2015)
     pol.implement_reform(Policy.read_json_reform(REFORM_JSON))
     syr = pol.start_year
-    # pylint: disable=protected-access
     amt_brk1 = pol._AMT_brk1
     assert amt_brk1[2015 - syr] == 200000
     assert amt_brk1[2016 - syr] > 200000
@@ -414,7 +412,7 @@ def test_implement_reform_raises_on_no_year():
     """
     reform = {'STD_Aged': [1400, 1200, 1400, 1400, 1400]}
     ppo = Policy()
-    with pytest.raises(pt.ValidationError):
+    with pytest.raises(paramtools.ValidationError):
         ppo.implement_reform(reform)
 
 
@@ -424,7 +422,7 @@ def test_implement_reform_raises_on_early_year():
     """
     ppo = Policy()
     reform = {'STD_Aged': {2010: [1400, 1100, 1100, 1400, 1400]}}
-    with pytest.raises(pt.ValidationError):
+    with pytest.raises(paramtools.ValidationError):
         ppo.implement_reform(reform)
 
 
@@ -817,18 +815,18 @@ def test_reform_with_removed_parameter(monkeypatch):
     """
     policy1 = Policy()
     reform1 = {'FilerCredit_c': {2020: 1000}}
-    with pytest.raises(pt.ValidationError):
+    with pytest.raises(paramtools.ValidationError):
         policy1.implement_reform(reform1)
     policy2 = Policy()
     reform2 = {'FilerCredit_c-indexed': {2020: True}}
-    with pytest.raises(pt.ValidationError):
+    with pytest.raises(paramtools.ValidationError):
         policy2.implement_reform(reform2)
 
     redefined_msg = {"some_redefined": "some_redefined was redefined."}
     monkeypatch.setattr(Policy, "REDEFINED_PARAMS", redefined_msg)
 
     pol = Policy()
-    with pytest.raises(pt.ValidationError):
+    with pytest.raises(paramtools.ValidationError):
         pol.implement_reform({"some_redefined": "hello world"})
 
 
@@ -871,28 +869,28 @@ def test_reform_with_scalar_vector_errors():
     """
     policy1 = Policy()
     reform1 = {'SS_thd85': {2020: 30000}}
-    with pytest.raises(pt.ValidationError):
+    with pytest.raises(paramtools.ValidationError):
         policy1.implement_reform(reform1)
 
     policy2 = Policy()
     reform2 = {'ID_Medical_frt': {2020: [0.08]}}
-    with pytest.raises(pt.ValidationError):
+    with pytest.raises(paramtools.ValidationError):
         policy2.implement_reform(reform2)
 
     policy3 = Policy()
     reform3 = {'ID_Medical_frt': [{"year": 2020, "value": [0.08]}]}
-    with pytest.raises(pt.ValidationError):
+    with pytest.raises(paramtools.ValidationError):
         policy3.adjust(reform3)
 
     # Check that error is thrown if there are extra elements in array.
     policy4 = Policy()
     ref4 = {"II_brk1": {2020: [9700, 19400, 9700, 13850, 19400, 19400]}}
-    with pytest.raises(pt.ValidationError):
+    with pytest.raises(paramtools.ValidationError):
         policy4.implement_reform(ref4)
 
     policy5 = Policy()
     ref5 = {"II_rt1": {2029: [.2, .3]}}
-    with pytest.raises(pt.ValidationError):
+    with pytest.raises(paramtools.ValidationError):
         policy5.implement_reform(ref5)
 
 
@@ -1403,10 +1401,7 @@ def test_multiple_cpi_swaps2():
     )
 
 
-# pylint: disable=invalid-name
-
-
-def test_adj_CPI_offset_and_index_status():
+def test_adj_cpi_offset_and_index_status():
     """
     Test changing parameter_indexing_CPI_offset and another
     parameter simultaneously.
@@ -1478,10 +1473,10 @@ def test_adj_related_parameters_and_index_status():
     )
     assert res.isel[0]["value"] == [445400]
 
-    II_brk7 = pol.to_array("II_brk7", year=[2021, 2022])
-    II_brk7_single = II_brk7[:, 0]
+    ii_brk7 = pol.to_array("II_brk7", year=[2021, 2022])
+    ii_brk7_single = ii_brk7[:, 0]
     np.testing.assert_equal(
-        (II_brk7_single[1] / II_brk7_single[0] - 1).round(4),
+        (ii_brk7_single[1] / ii_brk7_single[0] - 1).round(4),
         pol.inflation_rates(year=2021),
     )
 
@@ -1496,7 +1491,7 @@ def test_indexed_status_parsing():
     pol2.adjust({"EITC_c-indexed": False})
     cmp_policy_objs(pol1, pol2)
 
-    with pytest.raises(pt.ValidationError):
+    with pytest.raises(paramtools.ValidationError):
         pol2.adjust({"EITC_c-indexed": 123})
 
 
