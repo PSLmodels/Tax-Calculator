951d0560b6a0bfcac8f321effc43c42dcff02aa6
martinholmer <martin.holmer@gmail.com>
2026-08-01 16:07:38 -0400

    Eliminate cli/input_data_tests/*params* files

--- taxcalc/cli/input_data_tests/tests.sh
+++ taxcalc/cli/input_data_tests/tests.sh
@@ -5,16 +5,8 @@
 # See Makefile target idtest for usage.
 
 # use CPS input files
-tc cps.csv 2025 --numyears 11 --exact --params --tables --silent
+tc cps.csv 2025 --numyears 11 --exact --tables --silent
 for yr in {25..35}; do
-    diff -q cps-$yr-#-#-#-#-params.baseline cps-$yr-params.baseline
-    if [ $? -eq 0 ]; then
-        rm cps-$yr-#-#-#-#-params.baseline
-    fi
-    diff -q cps-$yr-#-#-#-#-params.reform cps-$yr-params.reform
-    if [ $? -eq 0 ]; then
-        rm cps-$yr-#-#-#-#-params.reform
-    fi
     diff -q cps-$yr-#-#-#-#.tables cps-$yr.tables
     if [ $? -eq 0 ]; then
         rm cps-$yr-#-#-#-#.tables
@@ -22,16 +14,8 @@ for yr in {25..35}; do
 done
 
 # use TMD input files
-tc ../../../tmd.csv 2025 --numyears 11 --exact --params --tables --silent
+tc ../../../tmd.csv 2025 --numyears 11 --exact --tables --silent
 for yr in {25..35}; do
-    diff -q tmd-$yr-#-#-#-#-params.baseline tmd-$yr-params.baseline
-    if [ $? -eq 0 ]; then
-        rm tmd-$yr-#-#-#-#-params.baseline
-    fi
-    diff -q tmd-$yr-#-#-#-#-params.reform tmd-$yr-params.reform
-    if [ $? -eq 0 ]; then
-        rm tmd-$yr-#-#-#-#-params.reform
-    fi
     diff -q tmd-$yr-#-#-#-#.tables tmd-$yr.tables
     if [ $? -eq 0 ]; then
         rm tmd-$yr-#-#-#-#.tables
